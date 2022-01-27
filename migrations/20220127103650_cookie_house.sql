c5c3332d52a2c23752774675ed414f4728464085
Frozen String <964413011@qq.com>
2022-01-27 19:07:43 +0800

    修改建表sql错误，生成entity

--- migrations/20220127103650_cookie_house.sql
+++ migrations/20220127103650_cookie_house.sql
@@ -2,7 +2,7 @@
 -- 根基表
 CREATE TABLE IF NOT EXISTS mansion(
     `id` BIGINT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    `create_time` DATETIME NOT NULL CURRENT_TIMESTAMP,
+    `create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `edit_time` DATETIME NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 -- 每天预测
@@ -12,14 +12,15 @@ CREATE TABLE IF NOT EXISTS each_mansion(
     -- 预测天
     `date` DATE NOT NULL UNIQUE,
     -- 预测内容动态（可能为空）
-    `content` VCHAR(512) DEFAULT NULL,
+    `content` VARCHAR(512) DEFAULT NULL,
     -- 约束区
     FOREIGN KEY(`mid`) REFERENCES mansion(id)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
 CREATE TABLE IF NOT EXISTS mansion_info(
     `id` BIGINT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
     `eid` BIGINT NOT NULL,
-    `predict_level` ENUM (`false`, `unknown`, `true`) NOT NULL,
-    `info` VCHAR(512) NOT NULL,
+    `predict_level` ENUM ('false', 'unknown', 'true') NOT NULL,
+    `info` VARCHAR(512) NOT NULL,
     FOREIGN KEY (`eid`) REFERENCES each_mansion(id)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
