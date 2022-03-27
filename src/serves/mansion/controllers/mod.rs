aefedb6717bd595a7e593b9fbdd958c09df4546a
Frozen String <964413011@qq.com>
2022-03-27 14:11:51 +0800

    :construction: 添加Mongodb 模式注册

--- src/serves/mansion/controllers/mod.rs
+++ src/serves/mansion/controllers/mod.rs
@@ -1,3 +1,11 @@
+use super::modules::{MansionDb, loading_model};
+
 pub mod mansion;
 
-crate::generate_controller!(MansionController, "/mansion", mansion::get_mansion);
+crate::generate_controller!(MansionController, "/mansion" ,mansion::save_mansion);
+
+crate::extra_module!(
+    MansionController=>crate::generate_collection_register!(
+        MansionDb=>loading_model
+    )
+);
