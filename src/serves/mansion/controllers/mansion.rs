c031a7023a19545e4cc77d2e38571f7df08e032a
Frozen String <964413011@qq.com>
2022-03-27 14:13:58 +0800

    :construction: 根据接口文档重构部分接口

--- src/serves/mansion/controllers/mansion.rs
+++ src/serves/mansion/controllers/mansion.rs
@@ -1,39 +1,36 @@
 use crate::{
     serves::mansion::{
-        db_ops::load_mansion::LoadMansion,
-        modules::mansion::{checkers::mansion::IdChecker, Mansion},
+        error::MansionError,
+        modules::mansion::{MansionCheckerPretreat, ViewMansion},
         MansionRResult,
     },
-    utils::{
-        data_checker::PretreatChecker,
-        req_pretreatment::{
-            db_operate::DbOp,
-            prefabs::{Null, PathValue, ToRResult},
-            ReqPretreatment,
-        },
+    utils::req_pretreatment::{
+        prefabs::{MapErr, ToRResult},
+        ReqPretreatment,
     },
 };
 use actix_web::{get, post, web};
 
-type LoadingTargetMansionFromDb = ReqPretreatment<
-    ToRResult<DbOp<LoadMansion, PretreatChecker<Null, PathValue<String>, IdChecker>>>,
->;
-
 crate::quick_struct! {
-    pub SaveMansionId{
+    pub MansionId{
+        #[serde(alias="idBefore",alias="mansionId")]
         id:Option<String>
     }
 }
 
-#[get("/{id}")]
-pub(super) async fn get_mansion(mansion: LoadingTargetMansionFromDb) -> MansionRResult<Mansion> {
-    mansion.unwrap()
-}
-
-#[post("/")]
+#[post("/upload")]
 pub(super) async fn save_mansion(
-    web::Query(SaveMansionId { id }): web::Query<SaveMansionId>,
+    web::Query(MansionId { id }): web::Query<MansionId>,
+    ReqPretreatment(json): ReqPretreatment<ToRResult<MapErr<MansionCheckerPretreat, MansionError>>>,
 ) -> MansionRResult<()> {
-    log::info!("saving id {:?}", id);
-    unimplemented!()
+    let data = json?;
+    
+    println!("{:#?}", data);
+    Ok(()).into()
+}
+#[get("/getInfo")]
+pub(super) async fn get_mansion(
+    web::Query(MansionId { id }): web::Query<MansionId>,
+) -> MansionRResult<ViewMansion> {
+    todo!("Get the mongodb and read data")
 }
