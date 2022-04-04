f151c2fdc5542c4b32e339aa69c4d70fbb941885
Frozen String <964413011@qq.com>
2022-04-04 10:40:51 +0800

    :loud_sound: 添加更多日志记录

--- libs/time-usage/src/async_time_usage.rs
+++ libs/time-usage/src/async_time_usage.rs
@@ -1,17 +1,24 @@
-use std::{future::Future, any::type_name, time::SystemTime};
+use std::{any::type_name, future::Future, time::SystemTime};
 
-
-pub async fn async_time_usage<F,Fut,R>(func:F)->R
-where F:FnOnce()->Fut,
-Fut:Future<Output = R>
+pub async fn async_time_usage<Fut, R>(fut: Fut) -> R
+where
+    Fut: Future<Output = R>,
 {
-    let name = type_name::<F>();
+    let name = type_name::<Fut>();
+    async_time_usage_with_name(name, fut).await
+}
+pub async fn async_time_usage_with_name<Fut, R>(
+    name: &'static str, fut: Fut,
+) -> R
+where
+    Fut: Future<Output = R>,
+{
     let start = SystemTime::now();
-    log::trace!("开始执行异步操作 {}", &name);
-    let r = func().await;
-    log::trace!("完成执行异步操作 {}", name);
+    log::trace!("开始执行异步操作 `{}`", &name);
+    let r = fut.await;
+    log::trace!("完成执行异步操作 `{}`", name);
     let end = SystemTime::now();
     let usage = end.duration_since(start).unwrap();
-    log::debug!("执行异步操作 {} 用时 {}ms", name, usage.as_millis());
+    log::debug!("执行异步操作 `{}` 用时 {}ms", name, usage.as_millis());
     r
 }
--- libs/time-usage/src/sync_time_usage.rs
+++ libs/time-usage/src/sync_time_usage.rs
@@ -1,17 +1,22 @@
 use std::{any::type_name, time::SystemTime};
 
-#[inline]
 pub fn sync_time_usage<F, R>(f: F) -> R
 where
     F: FnOnce() -> R,
 {
     let name = type_name::<F>();
+    sync_time_usage_with_name(name, f)
+}
+pub fn sync_time_usage_with_name<F, R>(name: &'static str, f: F) -> R
+where
+    F: FnOnce() -> R,
+{
     let start = SystemTime::now();
-    log::trace!("开始执行同步操作 {}", &name);
+    log::trace!("开始执行同步操作 `{}`", &name);
     let r = f();
-    log::trace!("完成执行同步操作 {}", name);
+    log::trace!("完成执行同步操作 `{}`", name);
     let end = SystemTime::now();
     let usage = end.duration_since(start).unwrap();
-    log::debug!("执行同步操作 {} 用时 {}ms", name, usage.as_millis());
+    log::debug!("执行同步操作 `{}` 用时 {}ms", name, usage.as_millis());
     r
 }
