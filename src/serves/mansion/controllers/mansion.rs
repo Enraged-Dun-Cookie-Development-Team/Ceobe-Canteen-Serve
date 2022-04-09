b6fac80c40de3bcb509273a0ab02ebb13255ccbe
lwt-414 <2957035701@qq.com>
2022-04-09 22:06:27 +0800

    ✨ 添加返回给前端饼学大厦信息接口

--- src/serves/mansion/controllers/mansion.rs
+++ src/serves/mansion/controllers/mansion.rs
@@ -13,7 +13,7 @@ use crate::{
     serves::mansion::{
         controllers::{MansionAuthentication, MidCheckerPretreatment},
         error::{MansionIdExist, MansionNotFound},
-        view::ViewMansion,
+        view::{ViewMansion, ViewMansionWithTime},
     },
     utils::req_pretreatment::ReqPretreatment,
 };
@@ -146,7 +146,7 @@ pub(super) async fn get_mansion(
 }
 
 #[get("/getId")]
-pub(super) async fn get_all_id(
+pub(super) async fn get_recent_id(
     _: MansionAuthentication, ReqPretreatment(db): MansionMongoDbPretreatment,
 ) -> MansionRResult<Vec<String>> {
     // 最近60天
@@ -215,4 +215,70 @@ pub(super) async fn remove_mansion(
     .await?;
 
     Ok(()).into()
+}
+
+
+/// 以下为前台获取
+#[get("/mansionInfo")]
+pub(super) async fn get_mansion_with_time(
+    ReqPretreatment(mid): MidCheckerPretreatment,
+    ReqPretreatment(db): MansionMongoDbPretreatment,
+) -> MansionRResult<ViewMansionWithTime> {
+    let MansionId { main_id, minor_id } = mid.id;
+    let db = db;
+
+    let filter = doc! {
+        "id" : {
+            "main_id":main_id
+            ,
+            "minor_id":minor_id as i32
+        }
+    };
+    let data = async_time_usage_with_name(
+        "查询Mansion信息",
+        db.doing::<_, ModelMansion, _, _>(|collect| {
+            async move { collect.find_one(filter, None).await }
+        }),
+    )
+    .await?
+    .ok_or(MansionNotFound)?;
+
+    MansionRResult::ok(data.into())
+}
+
+#[get("/mansionId")]
+pub(super) async fn get_all_id(
+    ReqPretreatment(db): MansionMongoDbPretreatment,
+) -> MansionRResult<Vec<String>> {
+    let filter = doc! {};
+
+    let resp = async_time_usage_with_name(
+        "获取MansionID列表（最近60天）",
+        db.doing::<_, ModelMansion, _, _>(|collect| {
+            async move {
+                let collect = collect.clone_with_type::<Mid>();
+                let mut vec = collect
+                    .find(
+                        filter,
+                        FindOptions::builder()
+                            .projection(doc! {"id":1i32})
+                            .sort(doc! {"id.main_id":1,"id.minor_id":1})
+                            .build(),
+                    )
+                    .await?;
+                let mut resp = Vec::new();
+                while let Some(v) = vec.next().await {
+                    let v = v?;
+                    resp.push(v);
+                }
+                Ok(resp)
+            }
+        }),
+    )
+    .await?
+    .into_iter()
+    .map(|id| id.id.to_string())
+    .collect();
+
+    Ok(resp).into()
 }
