740b504a03bf83d5aa19d09da074eaea41d939a6
lwt-414 <2957035701@qq.com>
2022-04-24 00:31:47 +0800

    🧑‍💻 将http_listen部分string替换为IpAddr与SocketAddr

--- src/configs/http_listen_config.rs
+++ src/configs/http_listen_config.rs
@@ -1,31 +1,30 @@
+use std::net::{IpAddr, SocketAddr};
+
 /// 用于构造http监听的配置文件信息
-pub trait HttpConfig: serde::de::DeserializeOwned {
-    fn host(&self) -> &str;
+pub trait HttpConfig {
+    fn host(&self) -> IpAddr;
     fn port(&self) -> u16;
-    fn url(&self) -> String;
+    fn url(&self) -> SocketAddr;
 }
 
 #[derive(Debug, serde::Deserialize)]
 pub struct HttpListenConfig {
     #[serde(default = "host_default")]
-    pub(crate) host: String,
+    pub(crate) host: IpAddr,
     #[serde(default = "port_default")]
     pub(crate) port: u16,
 }
 
 impl HttpConfig for HttpListenConfig {
-    fn host(&self) -> &str { &self.host }
+    fn host(&self) -> IpAddr { self.host }
+
     fn port(&self) -> u16 { self.port }
 
-    fn url(&self) -> String {
-        let url = format!(
-            "{host}:{port}",
-            host = &self.host,
-            port = self.port
-        );
+    fn url(&self) -> SocketAddr {
+        let url = SocketAddr::new(self.host, self.port);
         url
     }
 }
 
-fn host_default() -> String { "127.0.0.1".into() }
+fn host_default() -> IpAddr { "127.0.0.1".parse().unwrap() }
 fn port_default() -> u16 { 8000 }
