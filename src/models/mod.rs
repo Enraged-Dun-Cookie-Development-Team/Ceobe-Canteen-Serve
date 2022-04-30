cd90a2aff1eda7abfdacced7679c4a42fe7ca015
Frozen String <964413011@qq.com>
2022-04-30 12:46:16 +0800

    :alien: 修改原有 models 模块，保持外部代码兼容

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,8 +1,11 @@
-pub mod mansion;
-pub mod common;
+crate::generate_model_register!(RootModels);
 
+pub mod common {
+    pub mod sql {
+        pub use orm_migrate::sql_models::common::*;
+    }
+}
 
-crate::generate_model_register!(
-    RootModels,
-    mansion::MansionModel
-);
+pub mod mansion {
+    pub use mongo_migration::mongo_models::mansion::*;
+}
