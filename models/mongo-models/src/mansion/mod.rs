132688ac7e2f27e2b0fbb744f09fd62e6b6db0b0
Frozen String <964413011@qq.com>
2022-07-19 12:08:38 +0800

    ♻️ 处理mansion数据库读写报错

--- models/mongo-models/src/mansion/mod.rs
+++ models/mongo-models/src/mansion/mod.rs
@@ -5,3 +5,53 @@ pub mod operate;
 pub mod preludes {
     pub use super::{check::*, mongo_db::*};
 }
+use status_err::{ErrPrefix, HttpCode};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum MansionDataError {
+    #[error("Mongo数据库异常")]
+    Db(#[from] mongodb::error::Error),
+    #[error("饼学大厦id格式不正确")]
+    UnknownMansionId,
+    #[error("指定饼学大厦ID未找到")]
+    MansionNotFound,
+    #[error("指定ID的饼学大厦已经存在")]
+    MansionIdExist { mansion_id: String },
+}
+
+impl status_err::StatusErr for MansionDataError {
+    fn prefix(&self) -> ErrPrefix {
+        match self {
+            MansionDataError::Db(db) => db.prefix(),
+            
+            MansionDataError::UnknownMansionId => ErrPrefix::CHECKER,
+            MansionDataError::MansionNotFound => ErrPrefix::NOT_FOUND,
+            MansionDataError::MansionIdExist { mansion_id: _ } => {
+                ErrPrefix::CHECKER
+            }
+        }
+    }
+
+    fn code(&self) -> u16 {
+        match self {
+            MansionDataError::Db(db) => db.code(),
+            
+            MansionDataError::UnknownMansionId => 0x0002,
+            MansionDataError::MansionNotFound => 0x0001,
+            MansionDataError::MansionIdExist { mansion_id: _ } => 0x0008,
+        }
+    }
+
+    fn http_code(&self) -> HttpCode {
+        match self {
+            MansionDataError::Db(db) => db.http_code(),
+            
+            MansionDataError::UnknownMansionId => HttpCode::NOT_ACCEPTABLE,
+            MansionDataError::MansionNotFound => HttpCode::NOT_FOUND,
+            MansionDataError::MansionIdExist { mansion_id: _ } => {
+                HttpCode::CONFLICT
+            }
+        }
+    }
+}
