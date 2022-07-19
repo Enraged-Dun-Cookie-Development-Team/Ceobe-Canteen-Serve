8ca19d5b07f062b0b979eb78d7f2c6e8dd2c340b
Frozen String <964413011@qq.com>
2022-07-19 17:20:13 +0800

    🔥 (mongo) 删除不必要代码

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,8 +2,6 @@ pub mod data_checker;
 pub mod data_struct;
 pub mod field_with_struct_trait;
 pub mod middleware;
-pub mod mongodb_utils;
-pub mod mvc_utils;
 pub mod quick_struct;
 pub mod req_pretreatment;
 pub mod user_authorize;
