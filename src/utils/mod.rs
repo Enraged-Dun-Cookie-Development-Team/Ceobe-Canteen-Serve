e8d627d45e4bf5a6f0053765e8644bd9847e3be4
Frozen String <964413011@qq.com>
2022-07-19 17:20:22 +0800

    🐛

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,6 +2,7 @@ pub mod data_checker;
 pub mod data_struct;
 pub mod field_with_struct_trait;
 pub mod middleware;
+pub mod mvc_utils;
 pub mod quick_struct;
 pub mod req_pretreatment;
 pub mod user_authorize;
