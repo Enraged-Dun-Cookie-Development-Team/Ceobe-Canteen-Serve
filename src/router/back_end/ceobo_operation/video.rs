8115808d4998c30531603e2960123d5311e25bf9
Frozen String <964413011@qq.com>
2022-07-24 20:20:43 +0800

    ✨ （router） 添加路由

--- src/router/back_end/ceobo_operation/video.rs
+++ src/router/back_end/ceobo_operation/video.rs
@@ -1,7 +1,10 @@
-use axum::Router;
+use axum::{routing::get, Router};
+use tower::limit::ConcurrencyLimitLayer;
 
 pub struct CeoboOperationVideo;
 
-pub(super) fn video_router()->Router{
+pub(super) fn video_router() -> Router {
     Router::new()
+        .route("/detail", get(CeoboOperationVideo::get_video_detail))
+        .layer(ConcurrencyLimitLayer::new(1))
 }
