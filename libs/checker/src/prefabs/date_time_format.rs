ada39596377f9fa06bacd035555573b6009a2696
Frozen String <964413011@qq.com>
2022-07-24 18:34:53 +0800

    ✨ (date format) 添加日期格式相关检查器

---
+++ libs/checker/src/prefabs/date_time_format.rs
@@ -0,0 +1,53 @@
+use chrono::{NaiveDate, NaiveDateTime};
+use futures::future::{ready, Ready};
+
+use crate::Checker;
+
+const DATE_TIME_FORMAT: &str = "%Y-%m-%d %H:%M:%S";
+
+pub struct DateTimeFormatChecker;
+
+impl Checker for DateTimeFormatChecker {
+    type Args = ();
+    type Checked = NaiveDateTime;
+    type Err = chrono::ParseError;
+    type Fut = Ready<Result<Self::Checked, Self::Err>>;
+    type Unchecked = String;
+
+    fn check(_: Self::Args, uncheck: Self::Unchecked) -> Self::Fut {
+        ready(NaiveDateTime::parse_from_str(&uncheck, DATE_TIME_FORMAT))
+    }
+}
+
+const DATE_FORMAT: &str = "%Y-%m-%d";
+
+pub struct DateFormatChecker;
+
+impl Checker for DateFormatChecker {
+    type Args = ();
+    type Checked = NaiveDate;
+    type Err = chrono::ParseError;
+    type Fut = Ready<Result<Self::Checked, Self::Err>>;
+    type Unchecked = String;
+
+    fn check(_: Self::Args, uncheck: Self::Unchecked) -> Self::Fut {
+        ready(NaiveDate::parse_from_str(&uncheck, DATE_FORMAT))
+    }
+}
+
+const TIME_FORMAT :&str = "%H:%M:%S";
+
+
+pub struct TimeFormatChecker;
+
+impl Checker for TimeFormatChecker {
+    type Args = ();
+    type Checked = NaiveDate;
+    type Err = chrono::ParseError;
+    type Fut = Ready<Result<Self::Checked, Self::Err>>;
+    type Unchecked = String;
+
+    fn check(_: Self::Args, uncheck: Self::Unchecked) -> Self::Fut {
+        ready(NaiveDate::parse_from_str(&uncheck, TIME_FORMAT))
+    }
+}
--- libs/checker/src/prefabs/mod.rs
+++ libs/checker/src/prefabs/mod.rs
@@ -1,3 +1,4 @@
+pub mod date_time_format;
 pub mod collect_checkers;
 pub mod no_check;
 pub mod option_checker;
