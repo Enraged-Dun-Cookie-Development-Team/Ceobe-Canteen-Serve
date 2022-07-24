5b786685941cbf12eacaf36b183f010925563e72
Frozen String <964413011@qq.com>
2022-07-24 17:29:18 +0800

    ♻️ (video) 相关脚手架搭建

--- models/sql-models/src/ceobe_operation/video/checkers/video_data.rs
+++ models/sql-models/src/ceobe_operation/video/checkers/video_data.rs
@@ -1,12 +1,14 @@
 use checker::{check_obj, prefabs::no_check::NoCheck};
 use chrono::NaiveDateTime;
 use range_limit::limits::max_limit::MaxRangeLimit;
+use sea_orm::Set;
 use typed_builder::TypedBuilder;
 
 use super::{
     bv::{Bv, BvChecker},
     CheckError,
 };
+use crate::ceobe_operation::video::models::model_video;
 
 #[derive(Debug, TypedBuilder)]
 pub struct CeobeOperationVideo {
@@ -31,4 +33,54 @@ check_obj! {
         pub cover_image: MaxRangeLimit<String, 256>
     }
     err : CheckError
+}
+
+impl model_video::ActiveModel {
+    pub(in crate::ceobe_operation::video) fn from_video_data_with_order(
+        CeobeOperationVideo {
+            bv,
+            start_time,
+            over_time,
+            title,
+            author,
+            video_link,
+            cover_image,
+        }: CeobeOperationVideo,
+        order: i32,
+    ) -> Self {
+        Self {
+            bv: Set(bv.to_string()),
+            order: Set(order),
+            start_time: Set(start_time),
+            over_time: Set(over_time),
+            title: Set(title),
+            author: Set(author),
+            video_link: Set(video_link),
+            cover_image: Set(cover_image),
+            ..Default::default()
+        }
+    }
+
+    pub(in crate::ceobe_operation::video) fn update_with_video_and_order(
+        &mut self,
+        CeobeOperationVideo {
+            bv: _,
+            start_time,
+            over_time,
+            title,
+            author,
+            video_link,
+            cover_image,
+        }: CeobeOperationVideo,
+        order: i32,
+    ) {
+        self.order = Set(order);
+        self.start_time = Set(start_time);
+        self.over_time = Set(over_time);
+        self.title = Set(title);
+        self.author = Set(author);
+        self.video_link = Set(video_link);
+        self.cover_image = Set(cover_image);
+        self.soft_recover();
+    }
 }
--- models/sql-models/src/ceobe_operation/video/models/mod.rs
+++ models/sql-models/src/ceobe_operation/video/models/mod.rs
@@ -1,1 +1,13 @@
-pub mod ceobe_operation_video;
+use chrono::{Local, NaiveDateTime};
+
+pub mod model_video;
+
+pub(in crate::ceobe_operation::video) fn get_now_naive_date_time(
+) -> Option<Box<NaiveDateTime>> {
+    Box::new(Local::now().naive_local()).into()
+}
+
+pub(in crate::ceobe_operation::video) fn get_zero_data_time() -> NaiveDateTime
+{
+    NaiveDateTime::from_timestamp(0, 0)
+}
