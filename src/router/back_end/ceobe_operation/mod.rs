542c808782695441414505f92b0d11fd8be5b8cd
Frozen String <964413011@qq.com>
2022-08-01 09:23:19 +0800

    ✨ 添加版本号相关错误

--- src/router/back_end/ceobe_operation/mod.rs
+++ src/router/back_end/ceobe_operation/mod.rs
@@ -1,15 +1,21 @@
+mod version;
 use axum::Router;
 
-use self::{announcement::announcement_router, video::video_router};
+use self::{
+    announcement::announcement_router, version::version_router,
+    video::video_router,
+};
 
 mod announcement;
 mod video;
 
 pub use announcement::CeobeOperationAnnouncement;
+pub use version::CeobeOpVersion;
 pub use video::CeobeOperationVideo;
 
 pub(super) fn ceobe_operation_router() -> Router {
     Router::new()
         .nest("/announcement", announcement_router())
         .nest("/video", video_router())
+        .nest("/version", version_router())
 }
