1f4a7c85d18b16b7a6b73078dcdeaf61d4e35568
Frozen String <964413011@qq.com>
2022-08-01 21:11:48 +0800

    ♻️ update exist mongo migrate

--- migrate/mongo-migration/src/lib.rs
+++ migrate/mongo-migration/src/lib.rs
@@ -1,11 +1,19 @@
 mod migrations;
 
-use mongo_migrate_util::{MigrationTrait, MigratorTrait};
+use async_trait::async_trait;
+use mongo_migrate_util::{Manager, MigratorTrait};
+use mongo_models::bakery::mansion::operate::MongoErr;
 pub use mongo_models::{self, mongo_connection};
 pub struct Migrator;
 
+#[async_trait]
 impl MigratorTrait for Migrator {
-    fn migrators(&self) -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(migrations::bakery::mansion::Migration)]
+    async fn migrating(&self, manage: &mut Manager<'_>) -> Result<(), MongoErr> {
+        manage
+            .append(migrations::bakery::mansion::Migration)
+            .await?
+            ;
+
+        Ok(())
     }
 }
--- migrate/mongo-migration/src/migrations/bakery/mansion.rs
+++ migrate/mongo-migration/src/migrations/bakery/mansion.rs
@@ -1,25 +1,33 @@
 const MANSION_IDX_NAME: &str = "mansion_idx";
 const MANSION_CREATE_TIME_IDX_NAME: &str = "mansion_create_time_idx";
 
-use mongo_migrate_util::{Manager, MigrationTrait};
+use mongo_migrate_util::MigrationTrait;
 use mongo_models::bakery::mansion::preludes::*;
-use mongodb::{bson::doc, options::IndexOptions, IndexModel};
+use mongodb::{
+    bson::doc, options::IndexOptions, ClientSession, Collection, IndexModel,
+};
 
 pub struct Migration;
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
+    type Model = ModelMansion;
+
+    fn name(&self) -> &'static str { "bakery_mansion" }
+
     async fn migrate(
-        &self, manager: &Manager,
+        &self, mansion: &Collection<Self::Model>, session: &mut ClientSession,
     ) -> Result<(), mongodb::error::Error> {
-        let mansion = manager.collection::<ModelMansion, _>("bakery_mansion");
-        let all_idx = mansion.list_index_names().await.unwrap_or_default();
+        let all_idx = mansion
+            .list_index_names_with_session(session)
+            .await
+            .unwrap_or_default();
         log::info!(" all idx {:?}", all_idx);
 
         log::info!("注册 {} Index", MANSION_IDX_NAME);
         if !all_idx.contains(&MANSION_IDX_NAME.to_string()) {
             mansion
-                .create_index(
+                .create_index_with_session(
                     IndexModel::builder()
                         .keys(doc! {
                             "id.main_id":1i32,
@@ -33,13 +41,14 @@ impl MigrationTrait for Migration {
                         )
                         .build(),
                     None,
+                    session,
                 )
                 .await?;
         }
         log::info!("注册 {} Index", MANSION_IDX_NAME);
         if !all_idx.contains(&MANSION_CREATE_TIME_IDX_NAME.to_string()) {
             mansion
-                .create_index(
+                .create_index_with_session(
                     IndexModel::builder()
                         .keys(doc! {
                             "create_time":1i32
@@ -53,6 +62,7 @@ impl MigrationTrait for Migration {
                         )
                         .build(),
                     None,
+                    session,
                 )
                 .await?;
         }
