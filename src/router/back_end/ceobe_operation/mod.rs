1a1305d738c868ae055a4d2f019ede0e6a8df09b
Frozen String <964413011@qq.com>
2022-08-01 09:35:50 +0800

    🛂 为 ceobe op 路由添加鉴权中间件

--- src/router/back_end/ceobe_operation/mod.rs
+++ src/router/back_end/ceobe_operation/mod.rs
@@ -5,6 +5,11 @@ use self::{
     announcement::announcement_router, version::version_router,
     video::video_router,
 };
+use crate::{
+    middleware::authorize::AuthorizeLayer,
+    new_auth_level,
+    utils::user_authorize::auth_level::prefabs::{Chef, Cooker},
+};
 
 mod announcement;
 mod video;
@@ -18,4 +23,12 @@ pub(super) fn ceobe_operation_router() -> Router {
         .nest("/announcement", announcement_router())
         .nest("/video", video_router())
         .nest("/version", version_router())
+        .route_layer(AuthorizeLayer::<Cooker>::new())
+}
+
+new_auth_level! {
+    pub CeobeOperationAuth=>[
+        Chef
+        Cooker
+    ]
 }
