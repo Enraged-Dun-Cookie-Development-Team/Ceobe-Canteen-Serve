1b5bfc86530e15f4d5c9163d30ecee8234c5074d
phidiaLam <2957035701@qq.com>
2022-08-03 12:59:30 +0800

    ✨ 添加上传手机版本路由

--- src/router/back_end/ceobe_operation/version.rs
+++ src/router/back_end/ceobe_operation/version.rs
@@ -4,4 +4,5 @@ pub struct CeobeOpVersion;
 
 pub fn version_router() -> Router {
     Router::new().route("/plugin", post(CeobeOpVersion::update_plugin))
+    .route("/phone", post(CeobeOpVersion::create_app_version))
 }
