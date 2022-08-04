6ac4e15a5fee538b7b23e63bd1c39f97b26b13a8
phidiaLam <2957035701@qq.com>
2022-08-04 12:19:18 +0800

    ✨ 插入app版本信息前判断版本号是否存在

--- models/sql-models/src/ceobe_operation/app_version/operate/create.rs
+++ models/sql-models/src/ceobe_operation/app_version/operate/create.rs
@@ -1,7 +1,7 @@
 use sea_orm::ActiveModelTrait;
 use sql_connection::get_sql_database;
 
-use super::{CeobeOperationAppVersionSqlOperate, OperateResult};
+use super::{CeobeOperationAppVersionSqlOperate, OperateResult, OperateError};
 use crate::ceobe_operation::app_version::{
     checkers::app_version_data::CeobeOperationAppVersion,
     models::model_app_version,
@@ -10,10 +10,14 @@ use crate::ceobe_operation::app_version::{
 
 impl CeobeOperationAppVersionSqlOperate {
     pub async fn create_one_version(
-        version: CeobeOperationAppVersion,
+        version_info: CeobeOperationAppVersion,
     ) -> OperateResult<()> {
         let db = get_sql_database();
-        let active = ActiveModel::create_app_version(version);
+        // 判断版本是否已存在
+        if Self::is_exist_app_version(version_info.version.clone(), db).await? {
+            return Err(OperateError::AppVersionIdExist(version_info.version));
+        }
+        let active = ActiveModel::create_app_version(version_info);
         active.insert(db).await?;
         Ok(())
     }
