9e81472f9f66768515585027924b44e053ba2741
Frozen String <964413011@qq.com>
2022-08-09 17:07:08 +0800

    ♻️ 使用derive 替代原本手动实现status err

--- models/sql-models/src/ceobe_operation/resource/checkers/mod.rs
+++ models/sql-models/src/ceobe_operation/resource/checkers/mod.rs
@@ -2,11 +2,15 @@ pub mod resource_data;
 
 use std::convert::Infallible;
 
+use status_err::StatusErr;
 use thiserror::Error;
 pub use CheckError::*;
 
-#[derive(Debug, Error)]
-pub enum CheckError {}
+#[derive(Debug, Error, StatusErr)]
+pub enum CheckError {
+    #[error(transparent)]
+    StrLengthExceed(#[from] range_limit::Error),
+}
 
 impl From<Infallible> for CheckError {
     fn from(_: Infallible) -> Self { unreachable!() }
--- models/sql-models/src/ceobe_operation/resource/operate/mod.rs
+++ models/sql-models/src/ceobe_operation/resource/operate/mod.rs
@@ -2,31 +2,17 @@ pub mod create;
 pub mod retrieve;
 pub mod update;
 
-use status_err::{ErrPrefix, StatusErr};
+use status_err::StatusErr;
 use thiserror::Error;
 
 pub struct CeobeOperationResourceSqlOperate;
 
 pub use OperateError::*;
 
-#[derive(Debug, Error)]
+#[derive(Debug, Error, StatusErr)]
 pub enum OperateError {
     #[error("查询数据库异常: {0}")]
     Db(#[from] sea_orm::DbErr),
 }
 #[allow(dead_code)]
 type OperateResult<T> = Result<T, OperateError>;
-
-impl StatusErr for OperateError {
-    fn prefix(&self) -> ErrPrefix {
-        match self {
-            Db(inner) => inner.prefix(),
-        }
-    }
-
-    fn code(&self) -> u16 {
-        match self {
-            Db(inner) => inner.code(),
-        }
-    }
-}
