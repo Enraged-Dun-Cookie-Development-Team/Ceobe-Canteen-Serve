6ade202ac3f982a01695ddba8dd03ff45ffd5d69
Frozen String <964413011@qq.com>
2022-08-10 09:33:26 +0800

    ✨ 完成http 接口制作

--- src/serves/backend/ceobe_operation/resource/controller.rs
+++ src/serves/backend/ceobe_operation/resource/controller.rs
@@ -1,7 +1,62 @@
-use crate::router::CeobeOpResource;
+use std::time::Duration;
+
+use axum::headers::HeaderName;
+use axum_prehandle::{
+    prefabs::json::JsonPayload, PreHandling, PreRespHandling,
+};
+use modify_cache::{CacheMode, CheckModify};
+use orm_migrate::sql_models::ceobe_operation::resource::{
+    checkers::resource_data::{
+        CeobeOperationResourceChecker, CeobeOperationResourceUncheck,
+    },
+    operate::CeobeOperationResourceSqlOperate,
+};
+use resp_result::{FlagWarp, RespResult};
+
+use super::{
+    error::{ResourceError, ResourceRResult},
+    view_models::Resource,
+};
+use crate::{
+    router::CeobeOpResource,
+    utils::{
+        data_checker::PreLiteChecker, user_authorize::config::get_header_name,
+    },
+};
+
+type ResourceUploadCheck = PreLiteChecker<
+    JsonPayload<CeobeOperationResourceUncheck>,
+    CeobeOperationResourceChecker,
+    ResourceError,
+>;
 
 impl CeobeOpResource {
-    pub async fn upload_resource() {}
+    pub async fn upload_resource(
+        PreHandling(resource): PreRespHandling<ResourceUploadCheck>,
+    ) -> ResourceRResult<()> {
+        CeobeOperationResourceSqlOperate::update_resource(resource).await?;
+        RespResult::ok(())
+    }
 
-    pub async fn get_resource() {}
+    pub async fn get_resource(
+        mut modify: CheckModify,
+    ) -> ResourceRResult<FlagWarp<Option<Resource>>> {
+        modify
+            .cache_headers
+            .clean_content_local()
+            .add_vary_headers([HeaderName::from_static(get_header_name())])
+            .get_control()
+            .set_ty(CacheMode::Private)
+            .set_max_age(Duration::from_secs(4 * 60 * 60));
+
+        let resp =
+            CeobeOperationResourceSqlOperate::get_resource(|raa, cd| {
+                Resource::from((raa, cd))
+            })
+            .await?;
+
+        let (data, flags) = modify.check_modify(resp)?;
+
+        RespResult::ok(data).with_flags(flags)
+    }
 }
--- src/serves/backend/ceobe_operation/resource/error.rs
+++ src/serves/backend/ceobe_operation/resource/error.rs
@@ -1,1 +1,18 @@
+use axum::extract::rejection::JsonRejection;
+use orm_migrate::sql_models::ceobe_operation::resource::{
+    checkers::CheckError, operate::OperateError,
+};
+use resp_result::RespResult;
 
+use crate::error_generate;
+
+error_generate! {
+    pub ResourceError
+
+    Json = JsonRejection
+    Check = CheckError
+    DbOperate = OperateError
+    ModifyCheck = modify_cache::Error
+}
+
+pub type ResourceRResult<T> = RespResult<T, ResourceError>;
