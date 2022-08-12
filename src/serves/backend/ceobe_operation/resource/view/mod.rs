a6ea04fa8360ab2e9c4dbc013502f30560e908b3
phidiaLam <2957035701@qq.com>
2022-08-12 15:40:23 +0800

    ⚰️ 删除后台资源不需要的缓存相关代码

--- src/serves/backend/ceobe_operation/resource/view/mod.rs
+++ src/serves/backend/ceobe_operation/resource/view/mod.rs
@@ -13,23 +13,11 @@ use serde::Serialize;
 pub struct Resource {
     #[serde(rename = "resources")]
     resource_all_available: AllAvailable,
-    countdown: Vec<Countdown>,
-    #[serde(skip)]
-    modify_at: NaiveDateTime,
+    countdown: Vec<Countdown>
 }
 
 pub use all_available::AllAvailable;
 pub use countdown::Countdown;
-
-impl ModifyState for Resource {
-    type Identify = Self;
-
-    fn get_last_modify_time(&self) -> Option<Cow<'_, NaiveDateTime>> {
-        Some(Cow::Borrowed(&self.modify_at))
-    }
-
-    fn get_identify(&self) -> Cow<'_, Self::Identify> { Cow::Borrowed(self) }
-}
 
 impl
     From<(
@@ -43,18 +31,9 @@ impl
             Vec<model_resource::Countdown>,
         ),
     ) -> Self {
-        let modify_at = NaiveDateTime::max(
-            raa.modify_at,
-            cd.iter()
-                .map(|v| v.modify_at)
-                .max()
-                .unwrap_or_else(get_zero_data_time),
-        );
-
         Self {
             resource_all_available: raa.into(),
-            countdown: cd.into_iter().map(Into::into).collect(),
-            modify_at,
+            countdown: cd.into_iter().map(Into::into).collect()
         }
     }
 }
