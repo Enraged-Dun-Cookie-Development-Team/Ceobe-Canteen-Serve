9b41a0c60e384916d72d07f1138643473b5d4eaa
Frozen String <964413011@qq.com>
2022-09-11 20:08:36 +0800

    ♻️ (db_init) 调整trait 接口，添加辅助函数

--- database/database_initial/src/lib.rs
+++ database/database_initial/src/lib.rs
@@ -8,30 +8,61 @@ pub trait DatabaseInitialBasic {
     type Builder;
 }
 
-pub trait DatabaseInitialConnect: DatabaseInitialBasic {
-    /// start up params;
-    type ConnectParams;
+pub trait DatabaseInitialConnect<Params: 'static>:
+    DatabaseInitialBasic
+{
     /// future returning
     type ConnectFuture<'p>: Future<Output = Result<Self::Builder, Self::Error>>
         + 'p;
 
     /// connect to the database
-    fn start_connect(params: &Self::ConnectParams)
-        -> Self::ConnectFuture<'_>;
+    fn start_connect(params: &Params) -> Self::ConnectFuture<'_>;
 }
 
-pub trait DatabaseInitialMigration: DatabaseInitialBasic {
-    type MigrateParams;
+pub trait DatabaseInitialMigration<Params: 'static>:
+    DatabaseInitialBasic
+{
     type MigrateFuture<'s>: Future<Output = Result<Self::Builder, Self::Error>>
         + 's;
 
     /// apply the database migration
     fn apply_migration(
-        self, params: &Self::MigrateParams,
+        builder: Self::Builder, params: &Params,
     ) -> Self::MigrateFuture<'_>;
 }
 
 /// a trait that for database init itself
 pub trait DatabaseInitial: DatabaseInitialBasic + Sized {
-    fn build(self) -> Result<Self, Self::Error>;
+    type BuildResult;
+
+    fn build(
+        builder: Self::Builder,
+    ) -> Result<Self::BuildResult, Self::Error>;
+}
+
+pub async fn connect_db<D, C>(
+    connect_params: &C,
+) -> Result<D::BuildResult, D::Error>
+where
+    D: DatabaseInitialConnect<C> + DatabaseInitial,
+    C: 'static,
+{
+    let builder = D::start_connect(connect_params).await?;
+
+    D::build(builder)
+}
+
+pub async fn connect_db_with_migrate<D, C, M>(
+    connect_params: &C, migrate_params: &M,
+) -> Result<D::BuildResult, D::Error>
+where
+    D: DatabaseInitialConnect<C>
+        + DatabaseInitialMigration<M>
+        + DatabaseInitial,
+    C: 'static,
+    M: 'static,
+{
+    let builder = D::start_connect(connect_params).await?;
+    let builder = D::apply_migration(builder, migrate_params).await?;
+    D::build(builder)
 }
