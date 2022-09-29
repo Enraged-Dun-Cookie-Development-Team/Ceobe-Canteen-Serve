294c32f85ad2c4da7e1b8120a8e140df59bac3f3
phidiaLam <2957035701@qq.com>
2022-09-29 17:19:00 +0800

    🧑‍💻 使分页列表更加泛用

--- libs/page_size/src/database/mod.rs
+++ libs/page_size/src/database/mod.rs
@@ -1,11 +1,11 @@
 use sea_orm::{Select, EntityTrait, QuerySelect};
 
 pub trait OffsetLimit {
-    fn offset_limit(&self, page: usize, size: usize) -> Self;
+    fn offset_limit(self, page: usize, size: usize) -> Self;
 }
 
 impl<E: EntityTrait> OffsetLimit for Select<E> {
-    fn offset_limit(&self, page: usize, size: usize) -> Self {
-        self.clone().offset(((page-1)*size) as u64).limit((page*size) as u64)
+    fn offset_limit(self, page: usize, size: usize) -> Self {
+        self.offset(((page-1)*size) as u64).limit((page*size) as u64)
     }
 }
--- libs/page_size/src/response/mod.rs
+++ libs/page_size/src/response/mod.rs
@@ -16,16 +16,16 @@ pub struct ListWithPageInfo<T: Serialize + IntoIterator> {
 
 pub trait GenerateListWithPageInfo {
     type ListType: Serialize + IntoIterator;
-    fn generate_list_with_page_info(list: Self::ListType, page: usize, size: usize, count: usize) -> Self;
+    fn generate_list_with_page_info(self, page: usize, size: usize, count: usize) -> ListWithPageInfo<Self::ListType>;
 }
 
 
-impl<T:Serialize + IntoIterator> GenerateListWithPageInfo for ListWithPageInfo<T> {
+impl<T:Serialize + IntoIterator> GenerateListWithPageInfo for T {
     type ListType = T;
     /// 将列表，与分页信息存入一个结构体
-    fn generate_list_with_page_info(list: Self::ListType, page: usize, size: usize, count: usize) -> Self {
-        Self {
-            list,
+    fn generate_list_with_page_info(self, page: usize, size: usize, count: usize) -> ListWithPageInfo<Self::ListType> {
+        ListWithPageInfo {
+            list: self,
             page_size: PageInfo {
                 page,
                 size,
