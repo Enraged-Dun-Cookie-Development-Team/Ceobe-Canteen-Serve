ceb5ecc1ac697ae528513abe3471d2a092585e12
phidiaLam <2957035701@qq.com>
2022-10-19 09:27:38 +0800

    👔 初始化mongodb链接配置

--- src/bootstrap/init.rs
+++ src/bootstrap/init.rs
@@ -1,34 +1,39 @@
 use axum_starter::{prepare, PreparedEffect};
 use database_traits::initial::connect_db_with_migrate;
+use mongo_migration::mongo_connection::{self, MongoDbConfig, MongoDbError};
 use orm_migrate::{
-    sql_connection::{DbConfig, SqlDatabase, sea_orm::DbErr},
+    sql_connection::{sea_orm::DbErr, DbConfig, SqlDatabase},
     Migrator, MigratorTrait,
 };
 
 use crate::{
     bootstrap::create::create_default_user,
-    configs::{first_user::FirstUserConfig, resp_result_config::RespResultConfig, auth_config::AuthConfig, logger::LoggerConfig}, utils::user_authorize,
+    configs::{
+        auth_config::AuthConfig, first_user::FirstUserConfig,
+        logger::LoggerConfig, resp_result_config::RespResultConfig,
+    },
+    utils::user_authorize,
 };
 
 /// 日志配置
 #[prepare(LoggerRegister 'arg)]
-fn logger_register(logger: &'arg LoggerConfig) -> impl PreparedEffect {
+async fn logger_register(logger: &'arg LoggerConfig) -> impl PreparedEffect {
     logger.register_logger();
 }
 
 /// 请求返回resp配置
 #[prepare(RespConfig 'arg)]
-fn resp_conf(resp_result: &'arg RespResultConfig) -> impl PreparedEffect {
+async fn resp_conf(resp_result: &'arg RespResultConfig) -> impl PreparedEffect {
     resp_result::set_config(resp_result);
 }
 
 /// 鉴权配置
 #[prepare(BackAuthConfig 'arg)]
-fn backend_user_auth_conf(user_auth: &'arg AuthConfig) -> impl PreparedEffect {
+async fn backend_user_auth_conf(
+    user_auth: &'arg AuthConfig,
+) -> impl PreparedEffect {
     user_authorize::set_auth_config(user_auth);
 }
-
-
 
 /// 连接mysql数据库并且做一次migrate up
 #[prepare(SqlDatabaseConnect 'arg)]
@@ -43,6 +48,19 @@ async fn connect_sql_db_with_migrate<'arg>(
             Ok(())
         }
     })
+    .await?;
+    Ok(())
+}
+
+/// 连接mongodb数据库
+#[prepare(MongoDatabaseConnect 'arg)]
+async fn connect_mongo_db<'arg>(
+    mongodb: &'arg MongoDbConfig,
+) -> Result<impl PreparedEffect, MongoDbError> {
+    connect_db_with_migrate::<mongo_connection::DatabaseManage, _, _>(
+        mongodb,
+        mongo_migration::Migrator,
+    )
     .await?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::sync::Arc;
 use axum::{handler::Handler, Extension, Router};
 use axum_starter::ServerPrepare;
 use bootstrap::{
-    create::create_default_user, init::{SqlDatabaseConnect, RespConfig, LoggerRegister, BackAuthConfig},
+    create::create_default_user, init::{SqlDatabaseConnect, RespConfig, LoggerRegister, BackAuthConfig, MongoDatabaseConnect},
 };
 use configs::{
     http_listen_config::HttpConfig, GlobalConfig, CONFIG_FILE_JSON,
@@ -49,6 +49,7 @@ async fn main() {
         .append(RespConfig)
         .append(BackAuthConfig)
         .append(SqlDatabaseConnect)
+        .append(MongoDatabaseConnect)
         .prepare_start()
         .await
         .expect("准备启动服务异常")
