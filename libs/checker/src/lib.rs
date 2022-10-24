f76d9badca3686a6cfa0c1fc8e4e69a1b34e98ee
Frozen String <964413011@qq.com>
2022-10-24 22:04:23 +0800

    🎨 cargo fmt

--- libs/checker/src/lib.rs
+++ libs/checker/src/lib.rs
@@ -1,5 +1,5 @@
-mod check_prehandle;
 mod check_fut;
+mod check_prehandle;
 mod checker;
 mod checker_impls;
 mod codegen;
@@ -9,8 +9,9 @@ mod require_check;
 
 pub use check_fut::CheckFut;
 pub use check_obj_macro::{check_obj, check_obj as check_gen};
+pub use check_prehandle::PreLiteChecker;
 pub use checker_impls::CheckRefFut;
 pub use lite_args::LiteArgs;
 pub use require_check::CheckRequire;
-pub use check_prehandle::PreLiteChecker;
+
 pub use crate::checker::{Checker, LiteChecker, RefChecker};
