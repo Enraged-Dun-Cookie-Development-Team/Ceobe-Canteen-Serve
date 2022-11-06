e11f2ebe7117d0ddcecbc0894aa583ea40398019
Frozen String <964413011@qq.com>
2022-11-06 17:16:02 +0800

    ♻️ fix backend mansion

--- src/serves/backend/bakery_mansion/models/mod.rs
+++ src/serves/backend/bakery_mansion/models/mod.rs
@@ -1,18 +1,18 @@
-use axum_prehandle::prefabs::{json::JsonPayload, query::QueryParams};
+
+use checker::{QueryCheckExtract, JsonCheckExtract};
 use mongo_migration::mongo_models::bakery::mansion::checkers::{
-    id_checker::{MidChecker, MidUncheck, OpMidChecker, OpMidUncheck},
-    mansion::{MansionChecker, MansionUncheck},
+    id_checker::{MidChecker, OpMidChecker},
+    mansion::{MansionChecker},
 };
 
 use crate::{
     serves::backend::bakery_mansion::error::MansionError,
-    utils::data_checker::PreLiteChecker,
 };
 
-pub type MansionCheckerPretreat =
-    PreLiteChecker<JsonPayload<MansionUncheck>, MansionChecker, MansionError>;
 
-pub type MIdCheckerPretreat =
-    PreLiteChecker<QueryParams<MidUncheck>, MidChecker, MansionError>;
-pub type OptionMidCheckerPretreat =
-    PreLiteChecker<QueryParams<OpMidUncheck>, OpMidChecker, MansionError>;
+    pub type OptionMidCheckerPretreatment =
+    QueryCheckExtract<OpMidChecker, MansionError>;
+pub type MidCheckerPretreatment = QueryCheckExtract<MidChecker, MansionError>;
+
+pub type MansionBodyCheckerPretreatment =
+    JsonCheckExtract<MansionChecker, MansionError>;
