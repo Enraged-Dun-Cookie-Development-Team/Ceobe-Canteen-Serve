bec769820c2a23439f66d61cf6a2df01ec5cb228
Frozen String <964413011@qq.com>
2022-11-08 15:53:10 +0800

    🎨 fmt Cargo.toml

--- src/bootstrap/init/component_init.rs
+++ src/bootstrap/init/component_init.rs
@@ -3,18 +3,20 @@ use resp_result::ConfigTrait;
 
 use crate::utils::user_authorize::{self, config::AuthConfig};
 
-/// 请求rresult配置
+/// rresult配置
 #[prepare(RResultConfig 'arg)]
-fn resp_conf<C>(resp_result: &'arg C) -> impl PreparedEffect
+fn resp_conf<C>(
+    resp_result: &'arg C,
+) -> Result<impl PreparedEffect, resp_result::SetRespResultConfigureError>
 where
     C: ConfigTrait,
 {
-    resp_result::set_config(resp_result);
+    resp_result::try_set_config(resp_result)
 }
 
 /// 鉴权配置
 #[prepare(BackendAuthConfig 'arg)]
-fn backend_user_auth_conf<C>(user_auth: &'arg C) -> impl PreparedEffect
+fn backend_user_auth_conf<C>(user_auth: &'arg C)
 where
     C: AuthConfig,
 {
