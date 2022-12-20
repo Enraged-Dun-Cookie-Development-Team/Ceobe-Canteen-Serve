587e200705b53333be3565a74908e4da4ce79e87
lwt-414 <2957035701@qq.com>
2022-12-20 23:47:18 +0800

    🦺 验证平台配置相关字段

--- models/sql-models/src/fetcher/platform_config/checkers/mod.rs
+++ models/sql-models/src/fetcher/platform_config/checkers/mod.rs
@@ -1,31 +1,18 @@
 pub mod platform_config_data;
 
+use std::convert::Infallible;
+
 use thiserror::Error;
 
 use status_err::{ErrPrefix, StatusErr, HttpCode};
 pub use CheckError::*;
 
-#[derive(Debug, Error)]
+#[derive(Debug, Error, StatusErr)]
 pub enum CheckError {
-    
+    #[error("范围超出限制: {0}")]
+    LengthExceed(#[from] range_limit::Error),
 }
 
-impl StatusErr for CheckError {
-    fn prefix(&self) -> ErrPrefix {
-        match self {
-            
-        }
-    }
-
-    fn code(&self) -> u16 {
-        match self {
-            
-        }
-    }
-
-    fn http_code(&self) -> HttpCode {
-        match self {
-            
-        }
-    }
+impl From<Infallible> for CheckError {
+    fn from(_: Infallible) -> Self { unreachable!("enter Infallible error") }
 }
--- models/sql-models/src/fetcher/platform_config/checkers/platform_config_data.rs
+++ models/sql-models/src/fetcher/platform_config/checkers/platform_config_data.rs
@@ -1,23 +1,46 @@
 
-use checker::check_obj;
+use checker::{check_obj, prefabs::no_check::NoCheck};
+use range_limit::{RangeBoundLimit, limits::max_limit::MaxLimit};
+use sea_orm::Set;
 use typed_builder::TypedBuilder;
 
 use super::CheckError;
 use crate::fetcher::platform_config::models::model_platform_config;
 
+type MaxLimitString<const H: usize> = RangeBoundLimit<String, MaxLimit<H>>;
+
 #[derive(Debug, TypedBuilder)]
 pub struct FetcherPlatformConfig {
-    
+    pub type_id: String,
+    pub platform_name: String,
+    pub min_request_interval: i32
 }
 
-check_obj! {
-    #[derive(Debug,serde::Deserialize)]
-    pub struct FetcherPlatformConfigUncheck = FetcherPlatformConfigChecker > FetcherPlatformConfig{
-        
-    }
-    err: CheckError
+#[check_obj(
+    uncheck = FetcherPlatformConfigUncheck,
+    checked = FetcherPlatformConfig,
+    error = CheckError
+)]
+#[derive(serde::Deserialize, Debug)]
+pub struct FetcherPlatformConfigChecker {
+    pub type_id: MaxLimitString<64>,
+    pub platform_name: MaxLimitString<64>,
+    pub min_request_interval: NoCheck<i32>
 }
 
 impl model_platform_config::ActiveModel {
-    
+    pub(in crate::fetcher::platform_config) fn platform_config_into_active_model(
+        FetcherPlatformConfig {
+            type_id,
+            platform_name,
+            min_request_interval,
+        }: FetcherPlatformConfig,
+    ) -> Self {
+        Self {
+            type_id: Set(type_id),
+            platform_name: Set(platform_name),
+            min_request_interval: Set(min_request_interval),
+            ..Default::default()
+        }
+    }
 }
