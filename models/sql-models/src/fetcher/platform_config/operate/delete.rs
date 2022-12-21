2ceb40f81571ba04f02718ab7a463040e48ea1f2
phidiaLam <2957035701@qq.com>
2022-12-21 20:54:52 +0800

    🗃️ 平台配置数据表删除

--- models/sql-models/src/fetcher/platform_config/operate/delete.rs
+++ models/sql-models/src/fetcher/platform_config/operate/delete.rs
@@ -1,6 +1,27 @@
 
-use super::FetcherPlatformConfigSqlOperate;
+use sea_orm::{ConnectionTrait, DbErr, EntityTrait};
+use sql_connection::database_traits::get_connect::GetDatabaseConnect;
+use tracing::{instrument, info};
+
+use crate::fetcher::platform_config::models::model_platform_config;
+
+use super::{FetcherPlatformConfigSqlOperate, OperateResult};
 
 impl FetcherPlatformConfigSqlOperate {
-    
+    #[instrument(skip(db), ret)]
+    // 删除一个平台
+    pub async fn delete_one_platform_config<'db, D>(
+        db: &'db D, pid: i32,
+    ) -> OperateResult<()>
+    where
+        D: GetDatabaseConnect<Error = DbErr> + 'db,
+        D::Connect<'db>: ConnectionTrait,
+    {
+        info!(platform.id = pid);
+        let db = db.get_connect()?;
+
+        model_platform_config::Entity::delete_by_id(pid).exec(db).await?;
+
+        Ok(())
+    }
 }
