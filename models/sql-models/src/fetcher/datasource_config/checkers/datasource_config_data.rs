4602a247925b13fad7f0cfa5d5141b4aa9576b28
phidiaLam <2957035701@qq.com>
2022-12-21 23:57:01 +0800

    🦺 验证数据源配置数据与转换成ActiveModel

--- models/sql-models/src/fetcher/datasource_config/checkers/datasource_config_data.rs
+++ models/sql-models/src/fetcher/datasource_config/checkers/datasource_config_data.rs
@@ -1,13 +1,24 @@
 
-use checker::check_obj;
+use checker::{check_obj, prefabs::{option_checker::OptionChecker, url_checker::UrlChecker, no_check::NoCheck}};
+use range_limit::{RangeBoundLimit, limits::max_limit::MaxLimit};
+use sea_orm::{Unset, ActiveValue::NotSet, Set};
 use typed_builder::TypedBuilder;
+use url::Url;
+use uuid::Uuid;
 
 use super::CheckError;
 use crate::fetcher::datasource_config::models::model_datasource_config;
 
+type MaxLimitString<const H: usize> = RangeBoundLimit<String, MaxLimit<H>>;
+
 #[derive(Debug, TypedBuilder)]
 pub struct FetcherDatasourceConfig {
-    
+    pub id: Option<i32>,
+    pub platfrom: String, 
+    pub datasource: String,
+    pub nickname: String,
+    pub avatar: Url,
+    pub config: String,
 }
 
 #[check_obj(
@@ -17,9 +28,44 @@ pub struct FetcherDatasourceConfig {
 )]
 #[derive(Debug,serde::Deserialize)]
 pub struct FetcherDatasourceConfigChecker {
-        
+    pub id: OptionChecker<NoCheck<i32>>,
+    pub platfrom: MaxLimitString<64>, 
+    pub datasource: MaxLimitString<64>,
+    pub nickname: MaxLimitString<16>,
+    pub avatar: UrlChecker,
+    pub config: NoCheck<String>,
 }
 
 impl model_datasource_config::ActiveModel {
-    
+    /// FetcherDatasourceConfig转ActiveModel
+    /// 
+    /// id:
+    ///     - 如果id为null，则为新增，uuid新建
+    ///     - 如果id有值，这更改，uuid为空
+    pub(in crate::fetcher::datasource_config) fn datasource_config_into_active_model(
+        FetcherDatasourceConfig {
+            id,
+            platfrom,
+            datasource,
+            nickname,
+            avatar,
+            config,
+        }: FetcherDatasourceConfig,
+    ) -> Self {
+        Self {
+            id: match id {
+                Some(id) => Set(id),
+                None => NotSet,
+            },
+            platfrom: Set(platfrom),
+            datasource: Set(datasource),
+            nickname: Set(nickname),
+            avatar: Set(avatar.to_string()),
+            config: Set(config),
+            unique_id: match id {
+                Some(_) => NotSet,
+                None => Set(Uuid::new_v4()),
+            },
+        }
+    }
 }
--- models/sql-models/src/fetcher/datasource_config/checkers/mod.rs
+++ models/sql-models/src/fetcher/datasource_config/checkers/mod.rs
@@ -1,5 +1,7 @@
 pub mod datasource_config_data;
 
+use std::convert::Infallible;
+
 use thiserror::Error;
 
 use status_err::{ErrPrefix, StatusErr, HttpCode};
@@ -7,5 +9,13 @@ pub use CheckError::*;
 
 #[derive(Debug, Error, StatusErr)]
 pub enum CheckError {
-    
+    #[error("范围超出限制: {0}")]
+    LengthExceed(#[from] range_limit::Error),
+
+    #[error("Url格式异常 {0}")]
+    Url(#[from] url::ParseError),
+}
+
+impl From<Infallible> for CheckError {
+    fn from(_: Infallible) -> Self { unreachable!("enter Infallible error") }
 }
