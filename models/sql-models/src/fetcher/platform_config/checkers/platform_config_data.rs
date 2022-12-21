c0ea2101456bd42221fe5574d469b84e1e32327d
phidiaLam <2957035701@qq.com>
2022-12-21 23:49:42 +0800

    🧑‍💻 优化平台配置模型检查

--- models/sql-models/src/fetcher/platform_config/checkers/platform_config_data.rs
+++ models/sql-models/src/fetcher/platform_config/checkers/platform_config_data.rs
@@ -1,7 +1,7 @@
 
-use checker::{check_obj, prefabs::no_check::NoCheck};
+use checker::{check_obj, prefabs::{no_check::NoCheck, option_checker::OptionChecker}};
 use range_limit::{RangeBoundLimit, limits::max_limit::MaxLimit};
-use sea_orm::Set;
+use sea_orm::{Set, ActiveValue::NotSet};
 use typed_builder::TypedBuilder;
 
 use super::CheckError;
@@ -11,6 +11,7 @@ type MaxLimitString<const H: usize> = RangeBoundLimit<String, MaxLimit<H>>;
 
 #[derive(Debug, TypedBuilder)]
 pub struct FetcherPlatformConfig {
+    pub id: Option<i32>,
     pub type_id: String,
     pub platform_name: String,
     pub min_request_interval: i32
@@ -23,6 +24,7 @@ pub struct FetcherPlatformConfig {
 )]
 #[derive(serde::Deserialize, Debug)]
 pub struct FetcherPlatformConfigChecker {
+    pub id: OptionChecker<NoCheck<i32>>,
     pub type_id: MaxLimitString<64>,
     pub platform_name: MaxLimitString<64>,
     pub min_request_interval: NoCheck<i32>
@@ -31,16 +33,20 @@ pub struct FetcherPlatformConfigChecker {
 impl model_platform_config::ActiveModel {
     pub(in crate::fetcher::platform_config) fn platform_config_into_active_model(
         FetcherPlatformConfig {
+            id,
             type_id,
             platform_name,
             min_request_interval,
         }: FetcherPlatformConfig,
     ) -> Self {
         Self {
+            id: match id {
+                Some(id) => Set(id),
+                None => NotSet,
+            },
             type_id: Set(type_id),
             platform_name: Set(platform_name),
             min_request_interval: Set(min_request_interval),
-            ..Default::default()
         }
     }
 }
--- models/sql-models/src/fetcher/platform_config/operate/update.rs
+++ models/sql-models/src/fetcher/platform_config/operate/update.rs
@@ -1,10 +1,14 @@
-
-use sea_orm::{DbErr, ConnectionTrait, Set, ActiveModelTrait, IntoActiveModel};
-use sql_connection::database_traits::get_connect::GetDatabaseConnect;
-use tracing::{instrument, info};
-use crate::fetcher::platform_config::models::model_platform_config::{ActiveModel, self};
-use crate::fetcher::platform_config::operate::OperateResult;
+use crate::fetcher::platform_config::checkers::platform_config_data::{FetcherPlatformConfigChecker, FetcherPlatformConfig};
 use crate::fetcher::platform_config::models::model_platform_config::Model;
+use crate::fetcher::platform_config::models::model_platform_config::{
+    self, ActiveModel,
+};
+use crate::fetcher::platform_config::operate::OperateResult;
+use sea_orm::{
+    ActiveModelTrait, ConnectionTrait, DbErr, IntoActiveModel, Set,
+};
+use sql_connection::database_traits::get_connect::GetDatabaseConnect;
+use tracing::{info, instrument};
 
 use super::FetcherPlatformConfigSqlOperate;
 
@@ -12,7 +16,7 @@ impl FetcherPlatformConfigSqlOperate {
     // 更新平台配置到数据库
     #[instrument(ret, skip(db))]
     pub async fn update_platform_config<'db, D>(
-        db: &'db D, config: Model,
+        db: &'db D, config: FetcherPlatformConfig,
     ) -> OperateResult<()>
     where
         D: GetDatabaseConnect<Error = DbErr> + 'static,
@@ -26,7 +30,12 @@ impl FetcherPlatformConfigSqlOperate {
         );
 
         let db = db.get_connect()?;
-        config.into_active_model().update(db).await?;
+        let platform_config_active =
+            ActiveModel::platform_config_into_active_model(config);
+        platform_config_active
+            .into_active_model()
+            .update(db)
+            .await?;
 
         Ok(())
     }
