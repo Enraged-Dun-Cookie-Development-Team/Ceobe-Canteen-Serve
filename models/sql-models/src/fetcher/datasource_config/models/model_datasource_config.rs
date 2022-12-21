96860a65fe02dac399df275cdd8edd30fa889a18
phidiaLam <2957035701@qq.com>
2022-12-21 23:50:52 +0800

    🏷️ 为数据源配置设置submodel用于前后台

--- models/sql-models/src/fetcher/datasource_config/models/model_datasource_config.rs
+++ models/sql-models/src/fetcher/datasource_config/models/model_datasource_config.rs
@@ -1,16 +1,23 @@
 use sea_orm::{ entity::prelude::*, Set };
+use sub_model::SubModel;
 
-
-#[derive(Debug, Clone, PartialEq, Eq, DeriveEntityModel)]
+#[derive(Debug, Clone, PartialEq, Eq, DeriveEntityModel, SubModel)]
 #[sea_orm(table_name = "fetcher_datasource_config")]
+#[sub_model(
+    all(name = "BackendDatasource", extra(derive(sea_orm::FromQueryResult))),
+    none(name = "FrontendDatasource", extra(derive(sea_orm::FromQueryResult))),
+)]
 pub struct Model {
     #[sea_orm(primary_key)]
     pub id: i32,
     pub platfrom: String, 
     pub datasource: String,
+    #[sub_model(want("FrontendDatasource"))]
     pub nickname: String,
+    #[sub_model(want("FrontendDatasource"))]
     pub avatar: String,
     pub config: String,
+    #[sub_model(ignore("BackendDatasource"), want("FrontendDatasource"))]
     pub unique_id: Uuid
 }
 
