878df580351fc1a9092f2593105b65370dc71e2b
phidiaLam <2957035701@qq.com>
2022-12-21 20:54:24 +0800

    🗃️ 平台配置数据表新增

--- models/sql-models/src/fetcher/platform_config/operate/create.rs
+++ models/sql-models/src/fetcher/platform_config/operate/create.rs
@@ -1,6 +1,35 @@
+use crate::fetcher::platform_config::models::model_platform_config::ActiveModel;
+use sea_orm::{ActiveModelTrait, ConnectionTrait, DbErr};
+use sql_connection::database_traits::get_connect::GetDatabaseConnect;
+use tracing::{info, instrument};
 
-use super::FetcherPlatformConfigSqlOperate;
+use crate::fetcher::platform_config::{
+    checkers::platform_config_data::FetcherPlatformConfig,
+    models::model_platform_config,
+};
+
+use super::{FetcherPlatformConfigSqlOperate, OperateResult};
 
 impl FetcherPlatformConfigSqlOperate {
-    
+    // 保存平台配置到数据库
+    #[instrument(ret, skip(db))]
+    pub async fn create_platform_config<'db, D>(
+        db: &'db D, config: FetcherPlatformConfig,
+    ) -> OperateResult<()>
+    where
+        D: GetDatabaseConnect<Error = DbErr> + 'static,
+        D::Connect<'db>: ConnectionTrait,
+    {
+        info!(
+            config.name = config.platform_name,
+            config.type_id = config.type_id,
+            config.min_request_interval = config.min_request_interval
+        );
+        let db = db.get_connect()?;
+        let platform_config_active =
+            ActiveModel::platform_config_into_active_model(config);
+        platform_config_active.save(db).await?;
+
+        Ok(())
+    }
 }
