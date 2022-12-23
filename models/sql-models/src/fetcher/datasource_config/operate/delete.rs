d8f6f81ef1fa7f655dc280b78e99d606ee7d151a
phidiaLam <2957035701@qq.com>
2022-12-23 16:48:30 +0800

    🗃️ 数据源配置删除

--- models/sql-models/src/fetcher/datasource_config/operate/delete.rs
+++ models/sql-models/src/fetcher/datasource_config/operate/delete.rs
@@ -1,6 +1,27 @@
 
-use super::FetcherDatasourceConfigSqlOperate;
+use sea_orm::{ConnectionTrait, DbErr, EntityTrait};
+use sql_connection::database_traits::get_connect::GetDatabaseConnect;
+use tracing::{instrument, info};
+
+use crate::fetcher::datasource_config::models::model_datasource_config;
+
+use super::{FetcherDatasourceConfigSqlOperate, OperateResult};
 
 impl FetcherDatasourceConfigSqlOperate {
-    
+    #[instrument(skip(db), ret)]
+    // 删除一个平台
+    pub async fn delete_one_datasource_config<'db, D>(
+        db: &'db D, did: i32,
+    ) -> OperateResult<()>
+    where
+        D: GetDatabaseConnect<Error = DbErr> + 'db,
+        D::Connect<'db>: ConnectionTrait,
+    {
+        info!(datasource.id = did);
+        let db = db.get_connect()?;
+
+        model_datasource_config::Entity::delete_by_id(did).exec(db).await?;
+
+        Ok(())
+    }
 }
