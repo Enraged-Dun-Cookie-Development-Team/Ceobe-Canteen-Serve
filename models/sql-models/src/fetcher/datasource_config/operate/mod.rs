b0140112463a56d44da1841d73ee74a09934cf3a
phidiaLam <2957035701@qq.com>
2022-12-24 11:53:40 +0800

    🗃️ 验证多个id是否都在datasource表里面

--- models/sql-models/src/fetcher/datasource_config/operate/mod.rs
+++ models/sql-models/src/fetcher/datasource_config/operate/mod.rs
@@ -4,6 +4,7 @@ pub mod retrieve;
 pub mod delete;
 pub mod verify;
 
+use sea_orm::FromQueryResult;
 use thiserror::Error;
 use status_err::{ErrPrefix, StatusErr};
 
@@ -18,3 +19,9 @@ pub enum OperateError {
 }
 #[allow(dead_code)]
 type OperateResult<T> = Result<T, OperateError>;
+
+
+#[derive(FromQueryResult)]
+struct DatasourceCounts {
+    pub(crate) count: i64,
+}
--- models/sql-models/src/fetcher/datasource_config/operate/verify.rs
+++ models/sql-models/src/fetcher/datasource_config/operate/verify.rs
@@ -1,6 +1,39 @@
+use sea_orm::{Statement, ConnectionTrait, DbErr, EntityTrait, DbBackend, DatabaseBackend};
+use sql_connection::database_traits::get_connect::GetDatabaseConnect;
+use tracing::instrument;
 
-use super::FetcherDatasourceConfigSqlOperate;
+use crate::fetcher::datasource_config::models::model_datasource_config;
+
+use super::{FetcherDatasourceConfigSqlOperate, OperateResult, DatasourceCounts};
 
 impl FetcherDatasourceConfigSqlOperate {
-    
+    // 验证id数组是否都存在
+    #[instrument(ret, skip(db))]
+    pub async fn has_all_datasource_ids<'db, D>(
+        db: &'db D, ids: Vec<i32>,
+    ) -> OperateResult<bool>
+    where
+        D: GetDatabaseConnect<Error = DbErr> + 'static,
+        D::Connect<'db>: ConnectionTrait,
+    {
+        let mut sql = String::from("select count(B.id) from (");
+        for id in ids {
+            sql.push_str(&format!(r#" select {id} as id from dual union"#));
+        }
+        sql = sql.trim_end_matches("union").to_string();
+        sql.push_str(&format!(r#") B left join fetcher_datasource_config on fetcher_datasource_config.id = B.id where fetcher_datasource_config.id is null;"#));
+
+        let db = db.get_connect()?;
+        let resp = model_datasource_config::Entity::find()
+            .from_raw_sql(Statement::from_string(
+                DatabaseBackend::MySql,
+                sql
+            ))
+            .into_model::<DatasourceCounts>()
+            .one(db)
+            .await?
+            .unwrap();
+
+        Ok(resp.count == 0)
+    }
 }
