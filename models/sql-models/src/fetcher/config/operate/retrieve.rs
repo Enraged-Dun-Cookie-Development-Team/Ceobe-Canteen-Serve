a911120d4b98ada3df9555e29501b31aa1564cdd
phidiaLam <2957035701@qq.com>
2022-12-24 20:12:12 +0800

    🗃️ 蹲饼器配置表查询

--- models/sql-models/src/fetcher/config/operate/retrieve.rs
+++ models/sql-models/src/fetcher/config/operate/retrieve.rs
@@ -1,6 +1,68 @@
 
-use super::FetcherConfigSqlOperate;
+use sea_orm::{ConnectionTrait, DbErr, EntityTrait, QueryFilter, ColumnTrait, Condition};
+use smallvec::SmallVec;
+use sql_connection::database_traits::get_connect::GetDatabaseConnect;
+use tap::TapFallible;
+use tracing::{instrument, info, Span};
+
+use crate::fetcher::config::{models::model_config::{self, Model}, checkers::config_data::FetcherConfig};
+
+use super::{FetcherConfigSqlOperate, OperateResult};
 
 impl FetcherConfigSqlOperate {
-    
+    #[instrument(skip(db))]
+    /// 获取单个平台下的全部蹲饼器配置
+    pub async fn find_single_platform_config_list<'db, D>(
+        db: &'db D, platform: String
+    ) -> OperateResult<Vec<Model>>
+    where
+        D: GetDatabaseConnect<Error = DbErr> + 'db,
+        D::Connect<'db>: ConnectionTrait,
+    {
+        info!(
+            fetcherConfig.platform = platform,
+        );
+        let db = db.get_connect()?;
+        
+        Ok(model_config::Entity::find()
+            .filter(model_config::Column::Platform.eq(platform))
+            .all(db)
+            .await?).tap_ok(|list| {
+                Span::current()
+                .in_scope(||{
+                    info!(fetcherConfig.len = list.len());
+                });
+            })
+    }
+
+    #[instrument(skip(db))]
+    /// 获取多个平台下的全部蹲饼器配置
+    pub async fn find_multi_platforms_config_list<'db, D>(
+        db: &'db D, platforms: Vec<String>
+    ) -> OperateResult<Vec<Model>>
+    where
+        D: GetDatabaseConnect<Error = DbErr> + 'db,
+        D::Connect<'db>: ConnectionTrait,
+    {
+        info!(
+            fetcherConfig.platform.list = ?platforms,
+        );
+        let db = db.get_connect()?;
+        
+        // 用Or拼接多个平台条件
+        let mut condition = Condition::any();
+        for platform in platforms {
+            condition = condition.add(model_config::Column::Platform.eq(platform));
+        }
+        
+        Ok(model_config::Entity::find()
+            .filter(condition)
+            .all(db)
+            .await?).tap_ok(|list| {
+                Span::current()
+                .in_scope(||{
+                    info!(fetcherConfig.len = list.len());
+                });
+            })
+    }
 }
