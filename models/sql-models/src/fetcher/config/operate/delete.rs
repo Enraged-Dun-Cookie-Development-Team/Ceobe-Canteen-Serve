e44a74abc36b82205f255bb0a3cc21e3d24fda98
phidiaLam <2957035701@qq.com>
2022-12-24 18:08:14 +0800

    🗃️ 蹲饼器配置表删除操作

--- models/sql-models/src/fetcher/config/operate/delete.rs
+++ models/sql-models/src/fetcher/config/operate/delete.rs
@@ -1,6 +1,40 @@
+use crate::fetcher::config::models::model_config::{self, Entity};
+use sea_orm::{
+    ColumnTrait, ConnectionTrait, DbErr, EntityTrait, QueryFilter,
+    TransactionTrait,
+};
+use tracing::{info, instrument};
 
-use super::FetcherConfigSqlOperate;
+use super::{FetcherConfigSqlOperate, OperateResult};
 
 impl FetcherConfigSqlOperate {
-    
+    #[instrument(skip(db), ret)]
+    // 根据平台删除相关配置
+    pub async fn delete_fetcher_configs_by_platform<'db, D>(
+        db: &impl ConnectionTrait, platform: String,
+    ) -> OperateResult<()> {
+        info!(fetcherConfig.platform = platform);
+
+        model_config::Entity::delete_many()
+            .filter(model_config::Column::Platform.eq(platform))
+            .exec(db)
+            .await?;
+
+        Ok(())
+    }
+
+    #[instrument(skip(db), ret)]
+    // 根据数据源id删除相关配置
+    pub async fn delete_fetcher_configs_by_datasource_id<'db, D>(
+        db: &impl ConnectionTrait, did: i32,
+    ) -> OperateResult<()> {
+        info!(fetcherConfig.did = did);
+
+        model_config::Entity::delete_many()
+            .filter(model_config::Column::DatasourceId.eq(did))
+            .exec(db)
+            .await?;
+
+        Ok(())
+    }
 }
