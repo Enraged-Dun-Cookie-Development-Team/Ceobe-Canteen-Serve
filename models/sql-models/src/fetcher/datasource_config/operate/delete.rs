49179bbca271850ba147b0a0a460625b98063b1f
phidiaLam <2957035701@qq.com>
2022-12-26 19:52:59 +0800

    🧑‍💻 更改删除平台模型层的数据库传参

--- models/sql-models/src/fetcher/datasource_config/operate/delete.rs
+++ models/sql-models/src/fetcher/datasource_config/operate/delete.rs
@@ -10,16 +10,10 @@ use super::{FetcherDatasourceConfigSqlOperate, OperateResult};
 impl FetcherDatasourceConfigSqlOperate {
     #[instrument(skip(db), ret)]
     // 删除一个平台
-    pub async fn delete_one_datasource_config<'db, D>(
-        db: &'db D, did: i32,
-    ) -> OperateResult<()>
-    where
-        D: GetDatabaseConnect<Error = DbErr> + 'db,
-        D::Connect<'db>: ConnectionTrait,
-    {
+    pub async fn delete_one_datasource_config(
+        db: &impl ConnectionTrait, did: i32,
+    ) -> OperateResult<()> {
         info!(datasource.id = did);
-        let db = db.get_connect()?;
-
         model_datasource_config::Entity::delete_by_id(did).exec(db).await?;
 
         Ok(())
