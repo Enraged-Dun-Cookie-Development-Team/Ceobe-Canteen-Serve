7b2f05e89ea25ee39d6ed7c72737fb4f33c2a5a3
Frozen String <964413011@qq.com>
2022-12-29 20:18:27 +0800

    ✅ 添加测试代码

--- libs/checker/src/prefabs/no_remain_checker.rs
+++ libs/checker/src/prefabs/no_remain_checker.rs
@@ -4,9 +4,9 @@ use futures::future::{ready, Ready};
 
 use crate::RefChecker;
 
-pub struct NoRemain<const RHS: u64>;
+pub struct NoRemainChecker<const RHS: u64>;
 
-impl<const RHS: u64> RefChecker for NoRemain<RHS> {
+impl<const RHS: u64> RefChecker for NoRemainChecker<RHS> {
     type Args = ();
     type Err = HasRem<RHS>;
     type Fut = Ready<Result<(), Self::Err>>;
@@ -14,23 +14,44 @@ impl<const RHS: u64> RefChecker for NoRemain<RHS> {
 
     fn ref_checker(_: Self::Args, target: &Self::Target) -> Self::Fut {
         let rem = target.rem(RHS);
-        ready(
-            if rem == 0 {
-                Ok(())
-            }
-            else {
-                Err(HasRem {
-                    origin: *target,
-                    rem,
-                })
-            },
-        )
+        ready(if rem == 0 {
+            Ok(())
+        } else {
+            Err(HasRem {
+                origin: *target,
+                rem,
+            })
+        })
     }
 }
 
-#[derive(Debug, thiserror::Error)]
+#[derive(Debug, thiserror::Error, PartialEq)]
 #[error("`{origin}` Has Rem for `{DIV}` with `{rem}`")]
 pub struct HasRem<const DIV: u64> {
     origin: u64,
     rem: u64,
+}
+
+#[cfg(test)]
+mod test {
+    use crate::RefChecker;
+
+    use super::{HasRem, NoRemainChecker};
+
+    #[test]
+    fn test() {
+        let a = 1111000;
+        let _ = NoRemainChecker::<1000>::ref_checker((), &a)
+            .into_inner()
+            .unwrap();
+
+        let r = NoRemainChecker::<10000>::ref_checker((), &a).into_inner();
+        assert_eq!(
+            Err(HasRem::<10000> {
+                origin: a,
+                rem: a % 10000
+            }),
+            r
+        )
+    }
 }
