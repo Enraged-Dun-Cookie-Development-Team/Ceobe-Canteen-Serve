2d836a2320a6262fb3eff559293180c431b5725e
Frozen String <964413011@qq.com>
2023-01-07 20:55:31 +0800

    🗃️ 调整数据operate 接口

--- models/sql-models/src/fetcher/datasource_config/operate/delete.rs
+++ models/sql-models/src/fetcher/datasource_config/operate/delete.rs
@@ -7,7 +7,7 @@ use crate::fetcher::datasource_config::models::model_datasource_config;
 impl FetcherDatasourceConfigSqlOperate {
     #[instrument(skip(db), ret)]
     /// 删除一个平台
-    pub async fn delete_one_datasource_config(
+    pub async fn delete_one(
         db: &impl ConnectionTrait, did: i32,
     ) -> OperateResult<()> {
         info!(datasource.id = did);
