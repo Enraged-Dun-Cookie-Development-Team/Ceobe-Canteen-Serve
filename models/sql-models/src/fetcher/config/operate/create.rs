a831399320c4c7ac4575a1cfef6144df55a5dad7
Frozen String <964413011@qq.com>
2023-01-07 20:40:34 +0800

    🗃️ 调整 Fetcher config operate 接口

--- models/sql-models/src/fetcher/config/operate/create.rs
+++ models/sql-models/src/fetcher/config/operate/create.rs
@@ -1,42 +1,26 @@
-use sea_orm::{ConnectionTrait, DbErr, EntityTrait};
-use sql_connection::database_traits::get_connect::{
-    GetDatabaseTransaction, TransactionOps,
-};
-use tracing::{info, instrument};
+use sea_orm::{ConnectionTrait, EntityTrait, IntoActiveModel};
+
+use tracing::instrument;
 
 use super::{FetcherConfigSqlOperate, OperateResult};
 use crate::fetcher::config::{
     checkers::config_data::FetcherConfig, models::model_config,
-    operate::create::model_config::ActiveModel,
 };
 
 impl FetcherConfigSqlOperate {
-    #[instrument(skip(db, configs))]
+    #[instrument(skip(ctx, configs))]
     /// 新建单一平台蹲饼配置
-    pub async fn create_configs_by_platform<'db, D>(
-        db: &'db D, platform: String, configs: Vec<FetcherConfig>,
-    ) -> OperateResult<()>
-    where
-        D: GetDatabaseTransaction<Error = DbErr> + 'db,
-        D::Transaction<'db>: ConnectionTrait,
-    {
-        info!(fetcherConfig.platform = platform,);
-
+    pub async fn create_multi(
+        ctx: &impl ConnectionTrait, configs: Vec<FetcherConfig>,
+    ) -> OperateResult<()> {
         // model数组转换activeModel数组
-        let active_models = configs
-            .into_iter()
-            .map(ActiveModel::fetcher_config_into_active_model);
-
-        let ctx = db.get_transaction().await?;
-
-        // 删除数据库中该平台的所有配置
-        Self::delete_fetcher_configs_by_platform(&ctx, platform).await?;
+        let active_models =
+            configs.into_iter().map(IntoActiveModel::into_active_model);
         // 将该平台的配置写入库中
         model_config::Entity::insert_many(active_models)
-            .exec(&ctx)
+            .exec(ctx)
             .await?;
 
-        ctx.submit().await?;
         Ok(())
     }
 }
--- models/sql-models/src/fetcher/config/operate/delete.rs
+++ models/sql-models/src/fetcher/config/operate/delete.rs
@@ -7,8 +7,8 @@ use crate::fetcher::config::models::model_config;
 impl FetcherConfigSqlOperate {
     #[instrument(skip(db), ret)]
     /// 根据平台删除相关配置
-    pub async fn delete_fetcher_configs_by_platform<'db>(
-        db: &impl ConnectionTrait, platform: String,
+    pub async fn delete_by_platform(
+        db: &impl ConnectionTrait, platform: &str,
     ) -> OperateResult<()> {
         info!(fetcherConfig.platform = platform);
 
@@ -19,10 +19,21 @@ impl FetcherConfigSqlOperate {
 
         Ok(())
     }
+    /// 根据平台删除相关配置,但是有多个平台
+    pub async fn delete_by_all_platform(
+        db: &impl ConnectionTrait, platforms: impl IntoIterator<Item = &str>,
+    ) -> OperateResult<()> {
+        model_config::Entity::delete_many()
+            .filter(model_config::Column::Platform.is_in(platforms))
+            .exec(db)
+            .await?;
+
+        Ok(())
+    }
 
     #[instrument(skip(db), ret)]
     /// 根据数据源id删除相关配置
-    pub async fn delete_fetcher_configs_by_datasource_id<'db>(
+    pub async fn delete_by_datasource_id(
         db: &impl ConnectionTrait, did: i32,
     ) -> OperateResult<()> {
         info!(fetcherConfig.did = did);
