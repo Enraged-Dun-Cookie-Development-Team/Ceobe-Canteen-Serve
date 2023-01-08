7cdb077170d180286b746fe18e803c7d43265fb9
Frozen String <964413011@qq.com>
2023-01-08 22:05:23 +0800

    ♻️ 调整分页API 命名

--- libs/page_size/src/database/mod.rs
+++ libs/page_size/src/database/mod.rs
@@ -5,11 +5,12 @@ use sea_orm::{EntityTrait, QuerySelect, Select};
 use crate::request::PageSize;
 
 pub trait OffsetLimit {
-    fn offset_limit(self, page_size: PageSize) -> Self;
+    fn with_pagination(self, page_size: PageSize) -> Self;
 }
 
 impl<E: EntityTrait> OffsetLimit for Select<E> {
-    fn offset_limit(self, PageSize { page, size }: PageSize) -> Self {
+    /// 添加分页查询
+    fn with_pagination(self, PageSize { page, size }: PageSize) -> Self {
         self.offset(((page.deref() - 1) * size.deref()) as u64)
             .limit((page.deref() * size.deref()) as u64)
     }
