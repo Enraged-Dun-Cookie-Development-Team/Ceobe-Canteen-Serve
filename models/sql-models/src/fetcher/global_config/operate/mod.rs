38305150cca2c2a7d77c14ac4f41e50f3a39a2f6
Frozen String <964413011@qq.com>
2023-01-24 17:49:02 +0800

    ♻️ 调整Global 数据库接口

--- models/sql-models/src/fetcher/global_config/operate/mod.rs
+++ models/sql-models/src/fetcher/global_config/operate/mod.rs
@@ -1,12 +1,34 @@
 pub mod retrieve;
 pub mod update;
 
+use sql_connection::database_traits::{database_operates::sub_operate::SubOperate, get_connect::GetDatabaseConnect};
 use status_err::StatusErr;
 use thiserror::Error;
 
-pub struct FetcherGlobalConfigSqlOperate;
+use crate::fetcher::FetcherOperate;
 
-pub use OperateError::*;
+pub struct Global<'c,C>(&'c C);
+
+impl<'c, C> GetDatabaseConnect for Global<'c, C> 
+where C:GetDatabaseConnect
+{
+    type Connect<'s> = C::Connect<'s>
+    where
+        Self: 's;
+
+    fn get_connect(&self) -> &Self::Connect<'_> {
+        self.0.get_connect()
+    }
+}
+
+impl<'c, C> SubOperate<'c> for Global<'c, C> {
+    type Parent = FetcherOperate<'c,C>;
+
+    fn from_parent(parent: &'c mut Self::Parent) -> Self {
+        Self(parent.0)
+    }
+}
+
 
 #[derive(Debug, Error, StatusErr)]
 pub enum OperateError {
--- models/sql-models/src/fetcher/global_config/operate/retrieve.rs
+++ models/sql-models/src/fetcher/global_config/operate/retrieve.rs
@@ -2,21 +2,21 @@ use sea_orm::{ConnectionTrait, EntityTrait};
 use sql_connection::database_traits::get_connect::GetDatabaseConnect;
 use tracing::instrument;
 
-use super::{FetcherGlobalConfigSqlOperate, OperateResult};
+use super::{Global, OperateResult};
 use crate::fetcher::global_config::models::model_global_config::{
     self, Entity,
 };
 
-impl FetcherGlobalConfigSqlOperate {
+impl<'c,C> Global<'c,C>
+where C:GetDatabaseConnect,
+C::Connect<'c>:ConnectionTrait
+{
     #[instrument(ret, skip_all)]
-    pub async fn get_all<'db, D>(
-        db: &'db D,
+    pub async fn get_all(
+        &'c self,
     ) -> OperateResult<Vec<model_global_config::Model>>
-    where
-        D: GetDatabaseConnect + 'static,
-        D::Connect<'db>: ConnectionTrait,
     {
-        let db = db.get_connect();
+        let db = self.get_connect();
         // 查询所有的键值对
         Ok(Entity::find().all(db).await?)
     }
