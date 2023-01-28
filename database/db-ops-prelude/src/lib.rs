a27d9a62d3c5e8ab18594f34bf24ad848962b2f7
Frozen String <964413011@qq.com>
2023-01-28 19:35:20 +0800

    ✨ 添加组合数据库op 辅助create

---
+++ database/db-ops-prelude/src/lib.rs
@@ -0,0 +1,26 @@
+// common
+pub use futures;
+pub use chrono;
+pub use smallstr;
+pub use smallvec;
+pub use status_err::{ErrPrefix, HttpCode, StatusErr};
+pub use tap;
+pub use thiserror::Error as ThisError;
+pub use tracing;
+// common database
+pub use ::sql_models::{
+    get_now_naive_date_time, get_now_naive_date_time_value,
+    get_zero_data_time,
+    sql_connection::{ext_traits, sea_orm},
+};
+pub use database_traits::{database_operates, get_connect};
+
+pub use ::mongo_models::{mongo_connection,mongodb,RecordUnit};
+
+pub mod sql_models {
+    pub use ::sql_models::{admin_user, ceobe_operation, fetcher};
+}
+
+pub mod mongo_models {
+    pub use ::mongo_models::{bakery, ceobe_operation};
+}
