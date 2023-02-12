b8070c21652d34c9e89af411bd3134747cc1549e
phidiaLam <2957035701@qq.com>
2023-02-12 17:52:55 +0800

    🐛 修复uuid转换成bson::uuid失败

--- logic/ceobe_user_logic/src/implements.rs
+++ logic/ceobe_user_logic/src/implements.rs
@@ -2,13 +2,8 @@
 
 use mongo_models::{mongo_connection::MongoDatabaseOperate, ceobe::user::{operate::ToUserOperate, models::UserChecked, check::user_checker::UserChecker}, mongodb::bson};
 use sql_models::{
-    fetcher::{
-        datasource_config::{
-            checkers::FetcherDatasourceConfig,
-            models::model_datasource_config::DatasourcePlatform,
-            operate::Datasource,
-        }, ToFetcherOperate,
-    },
+    fetcher::
+        ToFetcherOperate,
     sql_connection::{
         database_traits::get_connect::{
             GetDatabaseConnect, GetDatabaseTransaction, TransactionOps,
@@ -17,7 +12,6 @@ use sql_models::{
     },
 };
 use mongo_models::ceobe::user::check::user_checker::UserUncheck;
-use uuid::{Builder, Uuid};
 use crate::{view::MobIdReq, error::LogicResult};
 use checker::LiteChecker;
 
@@ -35,15 +29,20 @@ impl CeobeUserLogic {
     {
         // TODO: 验证mob_id是否为小刻食堂旗下mob id
 
-        let datasource_uuids = db.fetcher_operate().datasource().find_all_uuid().await?.into_iter().map(|uuid| uuid.into()).collect::<Vec<bson::Uuid>>();
 
-        let userUncheck = UserUncheck::builder()
+        // 获取所有数据源的uuid列表
+        let datasource_uuids = db.fetcher_operate().datasource().find_all_uuid().await?.into_iter().map(|uuid| uuid.into()).collect::<Vec<bson::uuid::Uuid>>();
+
+        // 拼接数据
+        let user_uncheck = UserUncheck::builder()
             .mob_id(mob_id.mob_id)
             .datasource_push(datasource_uuids)
             .build();
-        let userChecked: UserChecked =
-            UserChecker::lite_check(userUncheck).await?;
-        mongo.user().create(userChecked);
+        // 验证数据
+        let user_checked: UserChecked =
+            UserChecker::lite_check(user_uncheck).await?;
+        // 将用户信息存入数据库
+        mongo.user().create(user_checked).await?;
         Ok(())
     }
 }
