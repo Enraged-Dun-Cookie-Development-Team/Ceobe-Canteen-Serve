94d3bdef289f056d2be5327a5ebe96cc2223a72f
phidiaLam <2957035701@qq.com>
2023-02-13 11:23:00 +0800

    🚩 为用户模型层添加builder模式

--- models/mongo-models/src/ceobe/user/check/user_checker.rs
+++ models/mongo-models/src/ceobe/user/check/user_checker.rs
@@ -1,9 +1,10 @@
-use checker::prefabs::{
+use checker::{prefabs::{
     collect_checkers::iter_checkers::IntoIterChecker, no_check::NoCheck,
-};
+}, ToCheckRequire};
 use mongodb::bson::Uuid;
 use range_limit::limits::max_limit::MaxRangeLimit;
 use serde::Deserialize;
+use typed_builder::TypedBuilder;
 
 use crate::ceobe::user::{check::CheckError, models::UserChecked};
 
@@ -12,8 +13,14 @@ use crate::ceobe::user::{check::CheckError, models::UserChecked};
     checked = UserChecked,
     error = CheckError
 )]
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, TypedBuilder)]
 pub struct UserChecker {
+    #[builder(setter(
+        transform = |id:String| ToCheckRequire::require_check(id)
+    ))]
     mob_id: MaxRangeLimit<String, 16>,
+    #[builder(setter(
+        transform = |datasource:Vec<Uuid>| ToCheckRequire::require_check(datasource)
+    ))]
     datasource_push: IntoIterChecker<Vec<Uuid>, NoCheck<Uuid>, Vec<Uuid>>,
 }
