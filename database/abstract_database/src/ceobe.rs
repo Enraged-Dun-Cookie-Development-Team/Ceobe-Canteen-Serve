1a1b946005d3557ab5cf9559fb8d854a93ef47e7
phidiaLam <2957035701@qq.com>
2023-03-02 23:37:12 +0800

    📦 打包abstract_database

---
+++ database/abstract_database/src/ceobe.rs
@@ -0,0 +1,25 @@
+use std::ops::Deref;
+
+use db_ops_prelude::database_operates::{DatabaseOperate, sub_operate::{SubOperate, SuperOperate}};
+
+pub struct CeobeDatabaseOperate<'db, Conn>(&'db Conn);
+
+impl<'db, Conn> Deref for CeobeDatabaseOperate<'db, Conn> {
+    type Target = Conn;
+
+    fn deref(&self) -> &Self::Target { self.0 }
+}
+
+impl<'db, Conn> SubOperate<'db> for CeobeDatabaseOperate<'db, Conn> {
+    type Parent = DatabaseOperate<Conn>;
+
+    fn from_parent(parent: &'db Self::Parent) -> Self { Self(parent) }
+}
+
+pub trait ToCeobe<C> {
+    fn ceobe(&self) -> CeobeDatabaseOperate<'_, C>;
+}
+
+impl<C> ToCeobe<C> for DatabaseOperate<C> {
+    fn ceobe(&self) -> CeobeDatabaseOperate<'_, C> { self.child() }
+}
