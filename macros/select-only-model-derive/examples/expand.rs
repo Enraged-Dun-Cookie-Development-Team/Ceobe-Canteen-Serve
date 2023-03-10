04344202ccbebca9befb6d7cf79b940686fab2e3
ForzenString <forzen-string@git.com>
2023-03-10 17:42:57 +0800

    refactor(macro) 调整宏展开内容

---
+++ macros/select-only-model-derive/examples/expand.rs
@@ -0,0 +1,13 @@
+use select_only_model_derive::SelectOnlyModel;
+
+use sql_models::ceobe_operation::announcement::Entity;
+fn main() {}
+
+#[allow(dead_code)]
+#[derive(Debug, SelectOnlyModel)]
+#[select_only(origin = "Entity")]
+struct AnnouncementContentImageOnly {
+    content: String,
+    #[select_only(from_col = "img_url")]
+    image: String,
+}
--- macros/select-only-model-derive/src/code_gen/field_conv.rs
+++ macros/select-only-model-derive/src/code_gen/field_conv.rs
@@ -1,6 +1,7 @@
 use darling::ToTokens;
-use quote::quote;
-use syn::{Expr, Ident, Type};
+use heck::ToUpperCamelCase;
+use quote::{format_ident, quote};
+use syn::{Ident, Type};
 
 use crate::derive_input::field::ModelFieldDefine;
 
@@ -12,38 +13,56 @@ pub(crate) enum SelectCols<'s> {
         &'s Type,
     ),
     ColAs(
+        &'s Type,
         // from col
-        &'s Expr, 
+        &'s Ident,
         // as field name
-        &'s Ident),
+        &'s Ident,
+    ),
 }
 
 impl<'s> SelectCols<'s> {
-    pub fn from(src:&'s ModelFieldDefine,entity:&'s Type)->Self{
-        if let Some(expr) = &src.from_col{
-            Self::ColAs(expr    , src.ident.as_ref().unwrap())
-        }
-        else {
+    pub fn from(src: &'s ModelFieldDefine, entity: &'s Type) -> Self {
+        if let Some(expr) = &src.from_col {
+            Self::ColAs(entity, expr, src.ident.as_ref().unwrap())
+        } else {
             Self::Col(src.ident.as_ref().unwrap(), entity)
         }
+    }
+
+    fn get_entity(&self) -> &'s Type {
+        match self {
+            SelectCols::Col(_, t) | SelectCols::ColAs(t, _, _) => t,
+        }
+    }
+
+    fn get_target_col(&self) -> Ident {
+        let v = match self {
+            SelectCols::Col(f, _) | SelectCols::ColAs(_, f, _) => f,
+        }
+        .to_string()
+        .to_upper_camel_case();
+        format_ident!("{v}")
     }
 }
 
 impl<'s> ToTokens for SelectCols<'s> {
     fn to_tokens(&self, tokens: &mut syn::__private::TokenStream2) {
+        let entity = self.get_entity();
+        let origin_col = self.get_target_col();
+        let col_value = quote!(
+            <#entity as ::sql_connection::EntityTrait>::Column :: #origin_col
+        );
         let token = match self {
-            SelectCols::Col(field, entity) => {
-                let col_name  = heck::AsUpperCamelCase(&field.to_string()).to_string();
-                let col_value = quote!( <#entity as EntityTrait>::Column:: #col_name );
-                quote!(.column(#col_value))
-            },
-            SelectCols::ColAs(from_col, as_field) => {
+            SelectCols::Col(..) => {
+                quote!(let selector = ::sql_connection::QuerySelect::column(selector, #col_value);)
+            }
+            SelectCols::ColAs(_, as_field, ..) => {
                 let s = as_field.to_string();
-                quote!(.column_as(#from_col, #s))
-            },
+                quote!(let selector = ::sql_connection::QuerySelect::column_as(selector, #col_value, #s);)
+            }
         };
 
         tokens.extend(token)
-
     }
 }
