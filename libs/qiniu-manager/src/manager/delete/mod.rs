8c7ddb0499073eeab435206cf2ff94cb038c5592
phidiaLam <2957035701@qq.com>
2023-03-12 19:04:43 +0800

    🧑‍💻 七牛云删除结果添加三次重试

--- libs/qiniu-manager/src/manager/delete/mod.rs
+++ libs/qiniu-manager/src/manager/delete/mod.rs
@@ -1,19 +1,31 @@
+use qiniu_objects_manager::apis::http_client::ResponseError;
 use tracing::info;
 
-use crate::{Manager, error};
+use crate::{Manager, error, Error};
 
 use super::ObjectName;
 
 impl Manager {
+    /// 删除对象储存文件，包含三次重试
     pub async fn delete(
         &self, object_name: impl ObjectName,
-    ) -> Result<(), error::Error> {
+    ) -> Result<(), Error> {
         info!(
             qiniu.bucket.delete = object_name.object_name(),
         );
 
         let bucket = &self.bucket;
-        bucket.delete_object(&object_name.object_name()).async_call().await?;
+        // 三次重试
+        let mut res_err = Option::<ResponseError>::None;
+        for _ in 0..3 {
+            res_err = bucket.delete_object(&object_name.object_name()).async_call().await.err();
+            if res_err.is_none() {
+                break;
+            }
+        }
+        if let Some(err) = res_err {
+            Err(err)?;
+        }
         
         Ok(())
     }
