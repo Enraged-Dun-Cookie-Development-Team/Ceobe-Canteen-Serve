63303c5bc5629a86e1daadc407b3afdde32a3279
phidiaLam <2957035701@qq.com>
2023-03-30 16:22:20 +0800

    👔 实现获取新饼逻辑层

--- logic/ceobe_cookie_logic/src/error.rs
+++ logic/ceobe_cookie_logic/src/error.rs
@@ -1,7 +1,13 @@
 use bitmap_convert::error::Error as BitmapConvError;
 use ceobe_cookie::temp_list::OperateError as TempListOperateError;
-use status_err::StatusErr;
+use mob_push_server::MobPushError;
+use status_err::{StatusErr, HttpCode, ErrPrefix};
 use thiserror::Error;
+use ceobe_user::property::OperateError as CeobeUserOperateError;
+use fetcher::{
+    datasource_config::OperateError as DatasourceOperateError,
+    datasource_combination::OperateError as DatasourceCombinationOperateError
+};
 
 #[derive(Debug, Error, StatusErr)]
 pub enum LogicError {
@@ -15,6 +21,26 @@ pub enum LogicError {
 
     #[error("Json 反/序列化失败 {0}")]
     Json(#[from] serde_json::Error),
+
+    #[error(transparent)]
+    #[status_err(err = "transparent")]
+    CeobeUserOperateError(#[from] CeobeUserOperateError),
+
+    #[error(transparent)]
+    #[status_err(err = "transparent")]
+    DatasourceOperateError(#[from] DatasourceOperateError),
+
+    #[error(transparent)]
+    #[status_err(err = "transparent")]
+    DatasourceCombinationOperateError(#[from] DatasourceCombinationOperateError),
+
+    #[error(transparent)]
+    #[status_err(err(
+        prefix = "ErrPrefix::CHECKER",
+        err_code = 0x0008,
+        http_code = "HttpCode::CONFLICT"
+    ))]
+    MobPushError(#[from] MobPushError),
 }
 
 pub(crate) type LogicResult<T> = Result<T, LogicError>;
--- logic/ceobe_cookie_logic/src/view.rs
+++ logic/ceobe_cookie_logic/src/view.rs
@@ -1,7 +1,17 @@
+
+use futures::{
+    future::{ready, Ready},
+    io::Cursor,
+};
+
+use ceobe_qiniu_upload::ObjectName;
+use mob_push_server::{PushEntity, push_notify::android::{Image, NotifyStyle, Warn, sound::WarnSound}};
 use mongo_migration::mongo_models::mongodb::bson::oid::ObjectId;
+use qiniu_cdn_upload::{update_payload::UploadPayload, update_source::UploadSource};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use typed_builder::TypedBuilder;
+use uuid::Uuid;
 
 // 分页饼列表返回模型
 #[derive(Debug, Clone, Serialize, Deserialize, TypedBuilder)]
@@ -15,4 +25,98 @@ pub struct CookieListResp {
 pub struct CookieListReq {
     pub datasource_comb_id: String,
     pub cookie_id: ObjectId,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, TypedBuilder)]
+pub struct CookieDatasourceReq {
+    pub datasource: String,
+    pub unique: String,
+}
+#[derive(Debug, Clone, Serialize, Deserialize, TypedBuilder)]
+pub struct CookieContentReq {
+    pub text: Option<String>,
+    pub image_url: Option<String>,
+}
+#[derive(Debug, Clone, Serialize, Deserialize, TypedBuilder)]
+pub struct NewCookieReq {
+    pub source: CookieDatasourceReq,
+    pub content: CookieContentReq,
+    pub cookie_id: ObjectId
+}
+
+pub struct PushInfo {
+    pub content: Option<String>,
+    pub datasource_name: String,
+    pub image_url: Option<String>,
+    pub icon_url: String,
+}
+
+impl PushEntity for PushInfo {
+    type Content = str;
+
+    fn get_send_content(&self) -> &Self::Content {
+        if let Some(content) = &self.content {
+            content
+        } else {
+            ""
+        }
+    }
+
+    fn get_title(&self) -> std::borrow::Cow<'_, str> { 
+        let name = &self.datasource_name;
+        format!(r#"小刻在【{name}】找到了一个饼！！"#).into() 
+    }
+
+    fn android_notify(&self, notify: &mut mob_push_server::push_notify::android::AndroidNotify) {
+        if let Some(image) = &self.image_url {
+            notify.set_notify_style(NotifyStyle::new_big_vision(image));
+        }
+        notify.set_image(Image::new_image(&self.icon_url));
+    }
+}
+
+// TODO: 以下两段和其他地方重复了，之后抽到service层
+pub struct DeleteObjectName {
+    pub file_name: String,
+}
+
+impl<'s> ObjectName<'s> for DeleteObjectName {
+    const DIR: Option<&'s str> = Some("datasource-comb");
+
+    fn file_name(&self) -> &str { &self.file_name }
+}
+/// 数据源组合id-最新饼id 上传对象储存
+#[derive(Debug, Clone, Serialize)]
+pub struct CombIdToCookieId {
+    pub cookie_id: Option<ObjectId>,
+}
+#[derive(Debug, Clone, Copy)]
+pub struct CombIdToCookieIdPlayLoad<'s> {
+    pub file_name: &'s str,
+}
+pub struct CombIdToCookieSource;
+
+impl<'s> UploadPayload for CombIdToCookieIdPlayLoad<'s> {
+    type Source = CombIdToCookieSource;
+
+    const DIR: &'static str = "datasource-comb";
+
+    fn obj_name(&self) -> &str { self.file_name }
+}
+
+impl UploadSource for CombIdToCookieSource {
+    type Error = serde_json::error::Error;
+    type Read = Cursor<Vec<u8>>;
+    type ReadFuture<'f> = Ready<Result<Self::Read, Self::Error>>;
+    type Source<'r> = &'r CombIdToCookieId;
+
+    fn read_data(payload: Self::Source<'_>) -> Self::ReadFuture<'_> {
+        ready(serde_json::to_vec(payload).map(Cursor::new))
+    }
+
+    fn content_type(
+        _payload: &Self::Source<'_>,
+    ) -> ceobe_qiniu_upload::mime_guess::Mime {
+        "application/json; charset=utf-8".parse().unwrap()
+    }
 }
