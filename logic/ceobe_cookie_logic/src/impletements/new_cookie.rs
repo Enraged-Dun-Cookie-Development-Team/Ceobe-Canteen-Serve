d0ee0cdf8094b3a46942e323d26d52eb4104c421
phidiaLam <2957035701@qq.com>
2023-03-31 20:18:10 +0800

    ⚡️ 新饼进来两个任务并行运行，提升接口速度

--- logic/ceobe_cookie_logic/src/impletements/new_cookie.rs
+++ logic/ceobe_cookie_logic/src/impletements/new_cookie.rs
@@ -6,6 +6,7 @@ use fetcher::{
     datasource_combination::DatasourceCombinationOperate,
     datasource_config::DatasourceOperate,
 };
+use futures::future;
 use mob_push_server::PushManager;
 use mongo_migration::mongo_connection::MongoDatabaseOperate;
 use qiniu_service::QiniuService;
@@ -32,40 +33,63 @@ impl CeobeCookieLogic {
                 &new_cookie.source.unique,
             )
             .await?;
-
-        // 查询用户列表
-        let user_list = mongo
-            .ceobe()
-            .user()
-            .property()
-            .find_user_list_by_datasource(datasource_info.unique_id.into())
-            .await?;
-
-        // 获取新饼需要改变的数据源组合
-        let comb_ids =
-            DatasourceCombinationOperate::find_comb_id_by_one_datasource_raw(
-                db,
-                datasource_info.id,
-            )
-            .await?;
-        // 更新最新饼id对象储存
-        // 删除对象储存中的数据源组合文件
-        QiniuService::update_multi_datasource_comb(&qiniu, Some(new_cookie.cookie_id.to_string()), comb_ids).await;
         
-        // mob推送新饼
-        let content = PushInfo::builder()
-            .content(new_cookie.content.text)
-            .datasource_name(datasource_info.nickname)
-            .image_url(new_cookie.content.image_url)
-            .icon_url(datasource_info.avatar)
-            .build();
+        let (datasource_error, qiniu_err) = future::join(
+            async {
+                // 查询用户列表
+                let result = mongo
+                    .ceobe()
+                    .user()
+                    .property()
+                    .find_user_list_by_datasource(datasource_info.unique_id.into())
+                    .await;
+                match result {
+                    Ok(user_list) => {
+                        // mob推送新饼
+                        let content = PushInfo::builder()
+                        .content(new_cookie.content.text)
+                        .datasource_name(datasource_info.nickname)
+                        .image_url(new_cookie.content.image_url)
+                        .icon_url(datasource_info.avatar)
+                        .build();
+                    
+                        if mob.mob_push::<_, String, _>(&content, &user_list)
+                            .await
+                            .is_err()
+                        {
+                            // TODO: qq频道告警
+                        }
+                    
+                        Ok(())
+                    },
+                    Err(err) => Err(err),
+                }
+            },
+            async {
+                // 获取新饼需要改变的数据源组合
+                let result =
+                DatasourceCombinationOperate::find_comb_id_by_one_datasource_raw(
+                    db,
+                    datasource_info.id,
+                )
+                .await;
+                match result {
+                    Ok(comb_ids) => {
+                        // 更新最新饼id对象储存
+                        // 删除对象储存中的数据源组合文件
+                        QiniuService::update_multi_datasource_comb(&qiniu, Some(new_cookie.cookie_id.to_string()), comb_ids).await;
+                        Ok(())
+                    },
+                    Err(err) => Err(err),
+                }
+            }
+        )
+        .await;
         
-        if mob.mob_push::<_, String, _>(&content, &user_list)
-            .await
-            .is_err()
-        {
-            // TODO: qq频道告警
-        }
+        datasource_error?; 
+        qiniu_err?;
+        
+        
 
         Ok(())
     }
