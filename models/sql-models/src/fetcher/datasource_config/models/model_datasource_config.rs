24f979309f210e3390088f2760d80f69218b603c
ForzenString <forzen-string@git.com>
2023-04-09 11:06:15 +0800

    Revert "fmt() cargo fm & cargo clippy"
    This reverts commit 11b47c48442cb42946b65ce10e9ec6dc0c3aad47.

--- models/sql-models/src/fetcher/datasource_config/models/model_datasource_config.rs
+++ models/sql-models/src/fetcher/datasource_config/models/model_datasource_config.rs
@@ -57,8 +57,7 @@ pub struct Model {
     /// 数据源名字
     #[sub_model(
         want("DataSourceForFetcherConfig"),
-        want("FrontendDatasource"),
-        want("NewCookiePushInfo")
+        want("FrontendDatasource"),want("NewCookiePushInfo")
     )]
     pub nickname: String,
     /// 数据源头像
@@ -102,7 +101,9 @@ impl RelationTrait for Relation {
 }
 
 impl Related<model_platform_config::Entity> for Entity {
-    fn to() -> RelationDef { Relation::PlatForm.def() }
+    fn to() -> RelationDef {
+        Relation::PlatForm.def()
+    }
 }
 
 impl ActiveModelBehavior for ActiveModel {}
