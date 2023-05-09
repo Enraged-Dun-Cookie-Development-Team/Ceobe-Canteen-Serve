181407fb421e9a4219bc62c1145aafb25fed86bc
phidiaLam <2957035701@qq.com>
2023-05-09 21:15:10 +0800

    🎨 cargo clippy

--- service/qiniu_service/src/impletements/datasource_comb.rs
+++ service/qiniu_service/src/impletements/datasource_comb.rs
@@ -38,8 +38,7 @@ impl QiniuService {
                         .manual()
                         .info("上传七牛云数据源对应最新饼id文件失败".into())
                         .extra(format!(
-                            "报错：{}\n组合id：{}\n饼id：{:#?}",
-                            err, comb_id, cookie_id
+                            "报错：{err}\n组合id：{comb_id}\n饼id：{cookie_id:#?}",
                         ))
                         .build(),
                 )
