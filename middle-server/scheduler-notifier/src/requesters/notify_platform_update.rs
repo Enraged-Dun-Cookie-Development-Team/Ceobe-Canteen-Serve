95f98543f10e321227a4e4a4b4a232f26e310afa
phidiaLam <2957035701@qq.com>
2023-05-10 02:43:06 +0800

    🐛 与调度器通讯从http2换到http1.1

--- middle-server/scheduler-notifier/src/requesters/notify_platform_update.rs
+++ middle-server/scheduler-notifier/src/requesters/notify_platform_update.rs
@@ -35,6 +35,7 @@ impl<'query> NotifyPlatformUpdate<'query> {
 
 impl<'query> Requester for NotifyPlatformUpdate<'query> {
     const METHOD: Method = Method::POST;
+    const VERSION: http::Version = http::Version::HTTP_11;
 
     fn get_url(&self) -> Url { self.url.to_owned() }
 
