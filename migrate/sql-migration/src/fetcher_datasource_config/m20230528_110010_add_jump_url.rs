3a9d21c1c4c4ef48f2f2e2b0a47d806afc4b6055
phidiaLam <2957035701@qq.com>
2023-05-28 11:29:19 +0800

    🎨 cargo clippy & fmt

--- migrate/sql-migration/src/fetcher_datasource_config/m20230528_110010_add_jump_url.rs
+++ migrate/sql-migration/src/fetcher_datasource_config/m20230528_110010_add_jump_url.rs
@@ -1,20 +1,19 @@
-
 use sea_orm_migration::prelude::*;
 
 use super::FetcherDatasourceConfig;
 
-
 pub struct Migration;
 impl MigrationName for Migration {
-    fn name(&self) -> &str { "m20230528_110010_fetcher_datasource_config_add_jump_url" }
+    fn name(&self) -> &str {
+        "m20230528_110010_fetcher_datasource_config_add_jump_url"
+    }
 }
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         let mut al = sea_query::Table::alter();
         al.table(FetcherDatasourceConfig::Table).add_column(
-            ColumnDef::new(FetcherDatasourceConfig::JumpUrl)
-                .string_len(256)
+            ColumnDef::new(FetcherDatasourceConfig::JumpUrl).string_len(256),
         );
         manager.alter_table(al).await?;
         Ok(())
