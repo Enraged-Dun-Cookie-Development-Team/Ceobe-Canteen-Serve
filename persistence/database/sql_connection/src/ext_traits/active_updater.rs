4ca995a223f1c30183b8116fb0234795c346efca
ForzenString <964413011@qq.com>
2023-08-04 19:59:32 +0800

    :sparkles: 将 with field 移动到单独文件

--- persistence/database/sql_connection/src/ext_traits/active_updater.rs
+++ persistence/database/sql_connection/src/ext_traits/active_updater.rs
@@ -1,6 +1,4 @@
-use sea_orm::{ActiveModelTrait, IntoActiveModel};
-use database_traits::has_field;
-use database_traits::has_scheme::{Field, Has};
+use sea_orm::{ActiveModelTrait};
 
 pub trait ActiveModelUpdater<A: ActiveModelTrait> {
     fn update_active(self, active_model: &mut A);
@@ -28,38 +26,3 @@ impl<T, A> UpdateActiveModel<A> for T
           A: ActiveModelUpdater<T>, {}
 
 
-pub struct UpdateWith<F: Field, A: Has<F>, T>(pub A::Ty, pub T);
-
-impl<F: Field, A: Has<F> + ActiveModelTrait, T: IntoActiveModel<A>> IntoActiveModel<A> for UpdateWith<F, A, T> {
-    fn into_active_model(self) -> A {
-        let mut this = self.1.into_active_model();
-        Has::set(&mut this, self.0);
-        this
-    }
-}
-
-impl<F, A, T> ActiveModelUpdater<A> for UpdateWith<F, A, T>
-    where A: ActiveModelTrait + Has<F>,
-          T: ActiveModelUpdater<A>,
-          F: Field {
-    fn update_active(self, active_model: &mut A) {
-        Has::<F>::set(active_model, self.0);
-        self.1.update_active(active_model);
-    }
-}
-
-
-pub trait With<A>: Sized {
-    fn with<F>(self,_:F, field: A::Ty) -> UpdateWith<F, A, Self>
-        where F: Field,
-        A:Has<F>
-    {
-        UpdateWith(field, self)
-    }
-}
-
-impl<T: Sized,A> With<A> for T {}
-
-
-has_field!(FieldOrder:order);
-pub type UpdateWithOrder<A, T> = UpdateWith<FieldOrder, A, T>;
--- persistence/database/sql_connection/src/ext_traits/mod.rs
+++ persistence/database/sql_connection/src/ext_traits/mod.rs
@@ -4,6 +4,7 @@ pub mod check_all_exist;
 pub mod counter;
 pub mod select_count;
 pub mod soft_delete;
+pub mod with_field;
 
 pub use active_updater::{ActiveModelUpdater, UpdateActiveModel};
 pub use counter::{Count, CountNonZero, CountZero};
