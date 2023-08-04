716a5cbf11e4049a0baecc74e416063ebefd0cc9
ForzenString <964413011@qq.com>
2023-08-04 17:43:08 +0800

    :bug: fix trait 实现错误

--- persistence/database/sql_connection/src/ext_traits/active_updater.rs
+++ persistence/database/sql_connection/src/ext_traits/active_updater.rs
@@ -1,27 +1,23 @@
 use sea_orm::ActiveModelTrait;
 
-pub trait ActiveModelUpdater {
-    type ActiveModel: ActiveModelTrait;
-
-    fn update_active(self, active_model: &mut Self::ActiveModel);
+pub trait ActiveModelUpdater<A: ActiveModelTrait> {
+    fn update_active(self, active_model: &mut A);
 }
 
-impl<A, F> ActiveModelUpdater for F 
-    where A: ActiveModelTrait, 
-          F: FnOnce(&mut A) 
+impl<A, F> ActiveModelUpdater<A> for F
+where
+    A: ActiveModelTrait,
+    F: FnOnce(&mut A),
 {
-    type ActiveModel = A;
-
-    fn update_active(self, active_model: &mut Self::ActiveModel) {
-        (self)(active_model)
-    }
+    fn update_active(self, active_model: &mut A) { (self)(active_model) }
 }
 
-
-pub trait UpdateActiveModel<Updater>: Sized where Updater: ActiveModelUpdater {
-    fn update(&mut self, updater: Updater) {
-        updater.update_active(self);
-    }
+pub trait UpdateActiveModel<Updater>
+where
+    Updater: ActiveModelUpdater<Self>,
+    Self: Sized + ActiveModelTrait,
+{
+    fn update(&mut self, updater: Updater) { updater.update_active(self); }
 
     fn chain_update(mut self, updater: Updater) -> Self {
         updater.update_active(&mut self);
