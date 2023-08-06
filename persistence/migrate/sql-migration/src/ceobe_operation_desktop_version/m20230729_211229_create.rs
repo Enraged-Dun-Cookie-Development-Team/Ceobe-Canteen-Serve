a367d5a37c895fe68bff00e8a0a84f6fbacd1bf8
ForzenString <964413011@qq.com>
2023-08-06 23:28:37 +0800

    :art: cargo fmt

--- persistence/migrate/sql-migration/src/ceobe_operation_desktop_version/m20230729_211229_create.rs
+++ persistence/migrate/sql-migration/src/ceobe_operation_desktop_version/m20230729_211229_create.rs
@@ -1,5 +1,7 @@
-use sea_orm_migration::{prelude::*, sea_orm::Statement};
-use sea_orm_migration::sea_orm::ConnectionTrait;
+use sea_orm_migration::{
+    prelude::*,
+    sea_orm::{ConnectionTrait, Statement},
+};
 
 pub struct Migration;
 impl MigrationName for Migration {
