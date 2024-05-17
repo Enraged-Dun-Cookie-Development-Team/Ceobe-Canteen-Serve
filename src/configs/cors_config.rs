6e289142f5cda1229c670e86078ff08f6cd36b69
洛梧藤 <2957035701@qq.com>
2024-05-17 20:16:11 +0800

    ♻️ 重构cors origin反序列化部分

--- src/configs/cors_config.rs
+++ src/configs/cors_config.rs
@@ -5,21 +5,31 @@ use crate::bootstrap::middleware::cors::CorsConfigTrait;
 
 #[derive(Deserialize, Debug, Clone, Default)]
 pub struct CorsConfigImpl {
-    #[serde(alias = "origins")]
-    allow_origins: Vec<String>,
+    #[serde(alias = "origins", deserialize_with = "de_origins")]
+    allow_origins: Vec<HeaderValue>,
     #[serde(alias = "methods", deserialize_with = "de_methods")]
     allow_methods: Vec<Method>,
 }
 
 impl CorsConfigTrait for CorsConfigImpl {
     fn allow_origins(&self) -> Vec<HeaderValue> {
-        self.allow_origins
-            .iter()
-            .map(|path| path.parse().expect("Bad Origin Value"))
-            .collect()
+        self.allow_origins.clone()
     }
 
     fn allow_methods(&self) -> Vec<Method> { self.allow_methods.clone() }
+}
+
+fn de_origins<'de, D: Deserializer<'de>>(
+    de: D,
+) -> Result<Vec<HeaderValue>, D::Error> {
+    let vec = Vec::<String>::deserialize(de)?;
+    vec
+        .iter()
+        .map(|path| path.parse())
+        .try_fold(Vec::with_capacity(vec.len()), |mut vec, value| {
+            vec.push(value.map_err(serde::de::Error::custom)?);
+            Ok(vec)
+        })
 }
 
 fn de_methods<'de, D: Deserializer<'de>>(
