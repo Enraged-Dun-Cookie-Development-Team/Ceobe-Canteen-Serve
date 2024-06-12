b4d20560ed51e06da20cf188373c67e1410fd693
ForzenString <964413011@qq.com>
2024-06-12 18:27:05 +0800

    :fire: remove outdate type

--- middle-server/tencent-cloud-server/src/cloud_manager/entities.rs
+++ middle-server/tencent-cloud-server/src/cloud_manager/entities.rs
@@ -1,10 +1,7 @@
 use std::fmt::{Display, Formatter};
 
-use chrono::Utc;
-use general_request_client::Method;
 use http::HeaderValue;
-use mime::Mime;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use smallstr::SmallString;
 use smallvec::SmallVec;
 use typed_builder::TypedBuilder;
@@ -14,37 +11,6 @@ pub type Sha256HexString = SmallString<[u8; 64]>;
 pub type HmacSha256Slice = SmallVec<[u8; 32]>;
 
 pub type PayloadBuffer = SmallVec<[u8; 32]>;
-
-#[derive(Debug, Clone, TypedBuilder)]
-pub struct CommonParameter {
-    pub service: Service,
-    pub version: &'static str,
-    pub action: &'static str,
-    #[builder(default)]
-    pub region: Option<String>,
-    #[builder(default = "TC3-HMAC-SHA256")]
-    pub algorithm: &'static str,
-    #[builder(default = Utc::now().timestamp())]
-    pub timestamp: i64,
-    #[builder(default = "content-type;host;x-tc-action")]
-    pub signed_headers: &'static str,
-    #[builder(default)]
-    pub token: Option<String>,
-}
-
-#[derive(Debug, Clone, TypedBuilder)]
-pub struct RequestContent<P, Q>
-where
-    P: Serialize,
-    Q: Serialize + Clone,
-{
-    #[builder(default = Method::POST)]
-    pub method: Method,
-    pub payload: P,
-    #[builder(default = Option::<Q>::None, setter(strip_option))]
-    pub query: Option<Q>,
-    pub content_type: Mime,
-}
 
 #[derive(Debug, Clone, TypedBuilder, Deserialize)]
 #[serde(rename_all = "PascalCase")]
