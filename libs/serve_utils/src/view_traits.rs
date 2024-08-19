8daad45fa744c8aeb705f63a35cdf2adbfc68a1d
ForzenString <964413011@qq.com>
2024-08-19 20:28:44 +0800

    :recycle: 调整接口

--- libs/serve_utils/src/view_traits.rs
+++ libs/serve_utils/src/view_traits.rs
@@ -3,18 +3,37 @@ use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use crate::view_traits::_private::SealTrait;
 
 pub trait OptionViewField: _private::SealTrait {
-    fn need_serde(&self) -> bool;
+    fn skip_serde(&self) -> bool;
 }
 
 mod _private {
     pub trait SealTrait {}
 }
 
-#[derive(Serialize, Deserialize, Default, Debug, Clone, Copy)]
+#[derive(Default, Debug, Clone, Copy)]
 pub struct SkipField;
 
+impl Serialize for SkipField {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_none()
+    }
+}
+
+impl<'de> Deserialize<'de> for SkipField {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let _ = <() as Deserialize<'de>>::deserialize(deserializer)?;
+        Ok(Self)
+    }
+}
+
 impl OptionViewField for SkipField {
-    fn need_serde(&self) -> bool { false }
+    fn skip_serde(&self) -> bool { true }
 }
 
 impl SealTrait for SkipField {}
@@ -45,13 +64,13 @@ impl<T: Serialize> Serialize for ValueField<T> {
 impl<T> SealTrait for ValueField<T> {}
 
 impl<T> OptionViewField for ValueField<T> {
-    fn need_serde(&self) -> bool { true }
+    fn skip_serde(&self) -> bool { false }
 }
 
 #[derive(Debug, Clone)]
 pub struct OptionValueField<T>(pub Option<T>);
 
-impl<'de, T: Deserialize<'de>> Deserialize for OptionValueField<T> {
+impl<'de, T: Deserialize<'de>> Deserialize<'de> for OptionValueField<T> {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: Deserializer<'de>,
@@ -72,7 +91,7 @@ impl<T: Serialize> Serialize for OptionValueField<T> {
 }
 
 impl<T> OptionViewField for OptionValueField<T> {
-    fn need_serde(&self) -> bool { self.0.is_some() }
+    fn skip_serde(&self) -> bool { self.0.is_none() }
 }
 
 impl<T> SealTrait for OptionValueField<T> {}
