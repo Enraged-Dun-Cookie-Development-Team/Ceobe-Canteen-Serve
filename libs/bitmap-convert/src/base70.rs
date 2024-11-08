08ff9d0db56db68e2e4381f02c717bb8f9650170
phidiaLam <2957035701@qq.com>
2024-11-08 22:48:44 +0800

    ⚡️ 提取base70转换中部分变量为只会赋值一次

--- libs/bitmap-convert/src/base70.rs
+++ libs/bitmap-convert/src/base70.rs
@@ -1,10 +1,25 @@
 use bitmaps::Bitmap;
 use bnum::types::U256;
+use once_cell::sync::Lazy;
 
 use crate::error::Error;
 
 const BASE_70: &str =
     "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-~._()!*";
+// 字符转下标
+static CHAR_TO_INDEX: Lazy<[u8; 127]> = Lazy::new(|| {
+    let mut char_to_index: [u8; 127] = [0; 127];
+    let index_to_char = BASE_70.chars().collect::<Vec<char>>();
+    for (i, c) in index_to_char.iter().enumerate() {
+        char_to_index[*c as usize] = i as u8;
+    }
+    char_to_index
+});
+// 下标转字符
+static INDEX_TO_CHAR: Lazy<Vec<char>> =
+    Lazy::new(|| BASE_70.chars().collect());
+// 字符数量
+static RADIX: Lazy<u32> = Lazy::new(|| INDEX_TO_CHAR.len() as u32);
 
 pub trait BitmapBase70Conv {
     type Error;
@@ -20,19 +35,15 @@ impl BitmapBase70Conv for Bitmap<256> {
     type Error = Error;
 
     fn to_base_70(&self) -> Result<String, Self::Error> {
-        // 将索引转换成字符串
-        let index_to_char: Vec<char> = BASE_70.chars().collect();
-        let radix = index_to_char.len() as u32;
-
         // 转换bitmap成u8数组
         let value = U256::from_radix_le(self.as_bytes(), 256)
             .ok_or(Error::LargeThen256)?;
-        let bytes = value.to_radix_le(radix);
+        let bytes = value.to_radix_le(*RADIX);
 
         // 转换为70进制的字符
         Ok(bytes
             .into_iter()
-            .map(|b| index_to_char[b as usize])
+            .map(|b| INDEX_TO_CHAR[b as usize])
             .collect())
     }
 
@@ -40,23 +51,15 @@ impl BitmapBase70Conv for Bitmap<256> {
     where
         Self: Sized,
     {
-        // 将字符串转换成索引
-        let mut char_to_index: [u8; 127] = [0; 127];
-        let index_to_char: Vec<char> = BASE_70.chars().collect();
-        let radix = index_to_char.len() as u32;
-        for (i, c) in index_to_char.into_iter().enumerate() {
-            char_to_index[c as usize] = i as u8;
-        }
-
         // 转换成u8数组
         let mut bytes: Vec<u8> = Vec::new();
         for c in string.chars() {
-            let index = char_to_index
+            let index = CHAR_TO_INDEX
                 .get(c as usize)
                 .ok_or(Error::NotConvertBitmap(string.clone()))?;
             bytes.push(*index);
         }
-        let value = U256::from_radix_le(&bytes, radix)
+        let value = U256::from_radix_le(&bytes, *RADIX)
             .ok_or_else(|| Error::NotConvertBitmap(string.clone()))?;
         let binding = value.to_radix_le(256);
         let bytes_not_complete = binding.as_slice();
