bd0bb7b03b31921679bef762566ba0869d7030f5
ForzenString <964413011@qq.com>
2025-04-29 10:36:59 +0800

    :bug: 修复管理员可以删除自身问题

--- src/serves/backend/user_auth/error.rs
+++ src/serves/backend/user_auth/error.rs
@@ -1,6 +1,7 @@
 use axum::extract::rejection::{JsonRejection, QueryRejection};
 use checker::prefabs::num_check::NonZeroUnsignedError;
 use crypto_str::inner_encoders::bcrypt::BcryptError;
+use status_err::status_error;
 
 crate::error_generate!(
     pub AdminUserError
@@ -10,4 +11,7 @@ crate::error_generate!(
     OrmDB = persistence::admin::user::OperateError
     Check = persistence::admin::models::CheckError
     PageSize = NonZeroUnsignedError
+    SelfDelete = SelfDeleteError
 );
+use status_err::ErrPrefix;
+status_error!(pub SelfDeleteError[ErrPrefix::CHECKER,0018]=>"正在试图抹除自身存在");
