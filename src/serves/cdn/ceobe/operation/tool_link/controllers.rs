ef193310f9c0dbdcb490c68f368d977cf8959d11
洛梧藤 <2957035701@qq.com>
2026-05-31 18:21:42 +0800

    ✨ 添加 ToolLinkKind 枚举及相关功能，优化工具链接处理逻辑

--- src/serves/cdn/ceobe/operation/tool_link/controllers.rs
+++ src/serves/cdn/ceobe/operation/tool_link/controllers.rs
@@ -1,21 +1,35 @@
+use axum::extract::Query;
 use axum_resp_result::resp_try;
 use ceobe_operation_logic::impletements::CeobeOperateLogic;
 use persistence::{
-    ceobe_operate::tool_link_mongodb::models::ToolLink,
+    ceobe_operate::tool_link_mongodb::models::{ToolLink, ToolLinkKind},
     mongodb::MongoDatabaseOperate,
 };
+use serde::Deserialize;
 use tracing::instrument;
 
 use super::error::CeobeToolLinkRResult;
 use crate::router::CdnOperateToolLinkFrontend;
 
+#[derive(Debug, Deserialize)]
+pub struct ToolLinkListQuery {
+    #[serde(default = "default_kinds")]
+    pub kind: Vec<ToolLinkKind>,
+}
+
+fn default_kinds() -> Vec<ToolLinkKind> {
+    vec![ToolLinkKind::Arknights]
+}
+
 impl CdnOperateToolLinkFrontend {
     #[instrument(ret, skip(mongo))]
     pub async fn list(
         mongo: MongoDatabaseOperate,
+        Query(query): Query<ToolLinkListQuery>,
     ) -> CeobeToolLinkRResult<Vec<ToolLink>> {
         resp_try(async {
-            Ok(CeobeOperateLogic::list_tool_link_mongo(mongo).await?)
+            Ok(CeobeOperateLogic::list_tool_link_mongo(mongo, query.kind)
+                .await?)
         })
         .await
     }
